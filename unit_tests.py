e2ad83b00c24cb7d0e2dd18013d1e8c65f5295ef
Orion (Theunkn0wn1) <theunkn0wn1@users.noreply.github.com>
2018-02-08 22:09:45 -0800

    added a test to prove proper on_message functionality for Commands

--- unit_tests.py
+++ unit_tests.py
@@ -64,5 +64,26 @@ class CommandTests(unittest.TestCase):
         with self.assertRaises(CommandNotFoundException):
             await Commands.trigger(message="!nope", sender="unit_test", channel="foo")
 
-    def test_find_command(self):
-        pass
+    @async_test
+    async def test_call_command(self):
+        """
+        Verifiy that found commands can be invoked via Commands.Trigger()
+        :return:
+        """
+        alias = ['potato', 'cannon', 'Fodder', 'fireball']
+        trigger_alias = [f"{Commands.prefix}{name}"for name in alias]
+        input_sender = "unit_test[BOT]"
+        input_channel = "unit_testing"
+        for name in alias:
+            @Commands.command(name)
+            async def potato(bot: pydle.Client, channel: str, sender: str):
+                print(f"bot={bot}\tchannel={channel}\tsender={sender}")
+                return bot, channel, sender
+
+        for command in trigger_alias:
+            with self.subTest(command=command):
+                outBot, outChannel, outSender = await Commands.trigger(message=command, sender=input_sender,
+                                                                       channel=input_channel)
+                self.assertEqual(input_sender, outSender)
+                self.assertEqual(input_channel, outChannel)
+                self.assertIsNotNone(outBot)
