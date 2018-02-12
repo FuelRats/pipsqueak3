7f5d992712a19a481ceaf55b96a880b6d944080c
Orion (Theunkn0wn1) <theunkn0wn1@users.noreply.github.com>
2018-02-12 13:13:05 -0800

    Further progress with permissions system. Fixed slight tab-complete blunder. TODO: unit tests

--- Modules/permissions.py
+++ Modules/permissions.py
@@ -20,7 +20,7 @@ log = logging.getLogger(f"{constants.base_logger}.Permissions")
 
 
 class Permission:
-    def __index__(self, level: int, vhost: str, deny_message: str="Access denied."):
+    def __init__(self, level: int, vhost: str, deny_message: str="Access denied."):
         """
         Permission required to execute a command
         :param level: Relitive permissions level
@@ -41,6 +41,31 @@ class Permission:
 
     def __ne__(self, other: 'Permission')->bool:
         return self.level != other.level
+    # TODO implement other relational operators
 
+
+class Permissions(enum):
+    """
+    Permission Enums.
+    """
+    RECRUIT = Permission(0, "recruit.fuelrats.com")
+    RAT = Permission(1, "rat.fuelrats.com")
+    DISPATCH = Permission(2, "dispatch.fuelrats.com")
+    OVERSEER = Permission(3, 'overseer.fuelrats.com')
+    OP = Permission(4, "op.fuelrats.com")
+    TECHRAT = Permission(5, 'techrat.fuelrats.com')
+    NETADMIN = Permission(6, 'netadmin.fuelrats.com')
+    ADMIN = Permission(6, 'admin.fuelrats.com')
+
+def RequirePermission(permission:Permissions, override_message: str or None = None):
+    """
+    Require an IRC command to be invoked by an authorized user.
+
+    Anything lower than the specifified permission will be rejected.
+    :param permission:
+    :return:
+    """
+    # TODO implement wrapper
+    pass
 
 
