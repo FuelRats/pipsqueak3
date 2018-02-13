49786f99717834468a6d876addc8352ac04785ed
Orion (Theunkn0wn1) <theunkn0wn1@users.noreply.github.com>
2018-02-12 19:40:09 -0800

    implemented Permissions UTs for the `Permission` class.

--- unit_tests.py
+++ unit_tests.py
@@ -2,15 +2,10 @@ import unittest
 from unittest import mock
 from Modules.rat_command import Commands, CommandNotFoundException, InvalidCommandException, NameCollisionException, \
     CommandException
+from Modules import permissions
 import pydle
 
 from aiounittest import async_test
-
-
-class MainTests(unittest.TestCase):
-    @unittest.expectedFailure
-    def test_run(self):
-        raise NotImplementedError("not implemented yet.")
 
 
 class RatCommandTests(unittest.TestCase):
@@ -171,3 +166,64 @@ class RatCommandTests(unittest.TestCase):
                 self.assertFalse(Commands._register(item, ['foo']))
 
 
+class PermissionTests(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def test_permission_greater(self):
+        """
+        Tests if Permission_a > Permission_b functions correctly.
+        :return:
+        """
+        self.assertTrue(permissions.RAT > permissions.RECRUIT)
+        self.assertTrue(permissions.ADMIN > permissions.RAT)
+        self.assertFalse(permissions.RAT > permissions.TECHRAT)
+
+    def test_permission_lesser(self):
+        """
+        Tests if Permission_a < Permission_b correctly.
+        :return:
+        """
+        self.assertTrue(permissions.RECRUIT < permissions.RAT)
+        self.assertTrue(permissions.RAT < permissions.TECHRAT)
+        self.assertFalse(permissions.ORANGE < permissions.TECHRAT)
+
+    def test_permission_le(self):
+        """
+        Tests if Permission_a <= Permission_b correctly.
+        :return:
+        """
+        self.assertTrue(permissions.ADMIN <= permissions.ADMIN)
+        self.assertFalse(permissions.ADMIN <= permissions.RAT)
+        self.assertTrue(permissions.ADMIN <= permissions.ORANGE)
+
+    def test_permission_ge(self):
+        """
+        Tests if Permission_a <= Permission_b correctly.
+        :return:
+        """
+        self.assertTrue(permissions.ADMIN >= permissions.ADMIN)
+        self.assertTrue(permissions.ADMIN >= permissions.RAT)
+        self.assertFalse(permissions.ADMIN >= permissions.ORANGE)
+
+    def test_permission_equal(self):
+        """
+        Verifies that Permission_a == Permission_b
+        :return:
+        """
+        self.assertTrue(permissions.RAT == permissions.RAT)
+        self.assertTrue(permissions.TECHRAT == permissions.TECHRAT)
+        self.assertTrue(permissions.ADMIN == permissions.NETADMIN)
+
+    def test_permission_not_equal(self):
+        """
+        Verifies that Permission_a != Permission_b
+        :return:
+        """
+        self.assertTrue(permissions.RAT != permissions.TECHRAT)
+        self.assertTrue(permissions.DISPATCH != permissions.ORANGE)
