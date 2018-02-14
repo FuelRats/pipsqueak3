f98cc0b9fd7126319f979cdef1b7708d54b9e227
Orion (Theunkn0wn1) <theunkn0wn1@users.noreply.github.com>
2018-02-14 12:56:07 -0800

    test commit

--- Modules/rat_command.py
+++ Modules/rat_command.py
@@ -1,6 +1,6 @@
 # coding: utf8
 """
-rat_command.py - Handles Command Events
+rat_command.py - Handles Command registration and Command-triggering IRC events
 
 Copyright (c) 2018 The Fuel Rats Mischief,
 All rights reserved.
