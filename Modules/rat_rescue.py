070465da897b428fab700da3cc6fa3a311a97428
theunkn0wn1 <thHunkn0WNd@gmail.com>
2018-02-17 15:48:04 -0800

    Added stub rat_rescue file.

---
+++ Modules/rat_rescue.py
@@ -0,0 +1,22 @@
+"""
+rat_rescue.py - Rescue board and objects
+
+Copyright (c) 2018 The Fuel Rats Mischief,
+All rights reserved.
+
+Licensed under the BSD 3-Clause License.
+
+See LICENSE.md
+
+This module is built on top of the Pydle system.
+"""
+
+
+class Rescue(object):
+    """
+    A unique rescue
+    """
+    def __init__(self):
+        """
+        Create a new rescue
+        """
