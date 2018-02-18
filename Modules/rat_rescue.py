c8dd928ab5377d034cbf204bbd196e5e49321537
theunkn0wn1 <thHunkn0WNd@gmail.com>
2018-02-17 23:03:23 -0800

    Expanded `Rescue` Documentation to describe parameters en verbatim

--- Modules/rat_rescue.py
+++ Modules/rat_rescue.py
@@ -18,13 +18,41 @@ log = logging.getLogger(f"{config.Logging.base_logger}.{__name__}")
 
 class Rescue(object):
     """
-    A unique rescue
+    A rescue
     """
     def __init__(self, case_id: str, client: str, system: str, created_at: datetime=None, updated_at: datetime=None,
                  unidentified_rats=None, active=True, quotes: list = None, is_open=True, epic=False, code_red=False,
                  successful=False, title='', first_limpet=None):
         """
-        Create a new rescue
+        A unique rescue\n
+        :param case_id: API id of rescue
+        :type case_id: str
+        :param client: The client's Commander name
+        :type client: str
+        :param system: The reported system for the rescue
+        :type system: str
+        :param created_at: Time the Rescue was first created ( READ ONLY once set)
+        :type created_at: datetime
+        :param updated_at: Last time the rescue was modified
+        :type updated_at: datetime
+        :param unidentified_rats: Assigned but unidentified rats
+        :type unidentified_rats: list
+        :param active: Bool determining if the case is marked as active
+        :type active: bool
+        :param quotes: Associated quotes for rescue
+        :type quotes: list
+        :param is_open: whether the case is marked as open
+        :type is_open: bool
+        :param epic: Is the rescue an Epic (NOT IMPLEMENTED YET)
+        :type epic: bool
+        :param code_red: is the rescue Code Red
+        :type code_red: bool
+        :param successful: was the rescue a success?
+        :type successful: bool
+        :param title: title for rescue, if any
+        :type title: str
+        :param first_limpet: Rat ID of the first-limpet
+        :type first_limpet: str
         """
         self._createdAt: datetime = created_at
         self._updatedAt: datetime = updated_at
