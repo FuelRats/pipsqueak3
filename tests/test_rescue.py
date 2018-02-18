a39ce83b4d64f7bdc356de3926e540fb9f080af2
theunkn0wn1 <thHunkn0WNd@gmail.com>
2018-02-17 23:03:23 -0800

    Expanded `Rescue` Documentation to describe parameters en verbatim

--- tests/test_rescue.py
+++ tests/test_rescue.py
@@ -1,10 +1,36 @@
+"""
+test_rescue.py - tests for Rescue and RescueBoard objects
+
+Copyright (c) 2018 The Fuel Rats Mischief,
+All rights reserved.
+
+Licensed under the BSD 3-Clause License.
+
+See LICENSE.md
+
+This module is built on top of the Pydle system.
+"""
+
 from unittest import TestCase
+
+from Modules.rat_rescue import Rescue
+from datetime import datetime
 
 
 class TestRescue(TestCase):
+    """
+    Tests for `Modules.Rescue.Rescue`
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.time = datetime.utcnow()
+
+        cls.rescue = Rescue("my-id", "stranded_commander", system="firestone", created_at=cls.time)
+
     def test_client_property_exists(self):
         """
-        Verifies the `Rescue.client` property exists
+        Verifies the `Rescue.client` property exists\n
         :return:
         :rtype:
         """
@@ -12,7 +38,7 @@ class TestRescue(TestCase):
 
     def test_client_writeable(self):
         """
-        Verifies the `Rescue.client` property can be written to correctly
+        Verifies the `Rescue.client` property can be written to correctly\n
         :return:
         :rtype:
         """
@@ -20,7 +46,7 @@ class TestRescue(TestCase):
 
     def test_created_at_creation(self):
         """
-        Verifies the `Rescue.created_at` property was correctly created
+        Verifies the `Rescue.created_at` property was correctly created\n
         :return:
         :rtype:
         """
@@ -28,7 +54,7 @@ class TestRescue(TestCase):
 
     def test_created_at_not_writable(self):
         """
-        Verifies the `Rescue.created_at` property cannot be written to
+        Verifies the `Rescue.created_at` property cannot be written to\n
         :return:
         :rtype:
         """
