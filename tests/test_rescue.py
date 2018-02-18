320aded706c82db1ddc8ccd4ae40d9eea71f88c6
theunkn0wn1 <thHunkn0WNd@gmail.com>
2018-02-17 22:49:33 -0800

    Add `Rescue` tests stub, implementation to follow

---
+++ tests/test_rescue.py
@@ -0,0 +1,47 @@
+from unittest import TestCase
+
+
+class TestRescue(TestCase):
+    def test_client_property_exists(self):
+        """
+        Verifies the `Rescue.client` property exists
+        :return:
+        :rtype:
+        """
+        self.fail()
+
+    def test_client_writeable(self):
+        """
+        Verifies the `Rescue.client` property can be written to correctly
+        :return:
+        :rtype:
+        """
+        self.fail()
+
+    def test_created_at_creation(self):
+        """
+        Verifies the `Rescue.created_at` property was correctly created
+        :return:
+        :rtype:
+        """
+        self.fail()
+
+    def test_created_at_not_writable(self):
+        """
+        Verifies the `Rescue.created_at` property cannot be written to
+        :return:
+        :rtype:
+        """
+        self.fail()
+
+    def test_system_initial_set(self):
+        """
+        Verifies the `Rescue.system` property was correctly set during init\n
+        :return:
+        :rtype:
+        """
+        self.fail()
+
+    def test_system_writable(self):
+        self.fail()
+
