c6144fa5875113259e147859db2963f5c417cdec
Orion (Theunkn0wn1) <theunkn0wn1@users.noreply.github.com>
2018-02-18 21:01:41 -0800

    added stub unit tests for quotes. TODO: implementation

---
+++ tests/test_quotes.py
@@ -0,0 +1,46 @@
+"""
+rat_rescue.py - Rescue board and objects
+
+Copyright (c) 2018 The Fuel Rats Mischief,
+All rights reserved.
+
+Licensed under the BSD 3-Clause License.
+
+See LICENSE.md
+
+This module is built on top of the Pydle system.
+"""
+from unittest import TestCase, expectedFailure
+
+
+@expectedFailure
+class TestQuotes(TestCase):
+    def test_message(self):
+        self.fail()
+
+    def test_message_setter(self):
+        self.fail()
+
+    def test_author(self):
+        self.fail()
+
+    def test_created_at(self):
+        self.fail()
+
+    def test_updated_at(self):
+        self.fail()
+
+    def test_updated_at_setter(self):
+        self.fail()
+
+    def test_last_author(self):
+        self.fail()
+
+    def test_last_author_setter(self):
+        self.fail()
+
+    def test_modify(self):
+        self.fail()
+
+    def test_new(self):
+        self.fail()
