00231591e30f8f36aa0aeacbb834bde55fe117c5
Orion (Theunkn0wn1) <theunkn0wn1@users.noreply.github.com>
2018-03-25 11:44:30 -0700

    Implemented `RatBoard.__contains__()`

--- Modules/Rat_Board.py
+++ Modules/Rat_Board.py
@@ -66,6 +66,24 @@ class RatBoard(object):
         self._rescues = {}
         """Rescue objects tracked by this board"""
 
+    def __contains__(self, item: Rescue) -> bool:
+        """
+        Checks if a rescue exists on this RescueBoard
+
+        Args:
+            item (Rescue):
+
+        Returns:
+            bool: if item exists
+        """
+        if not isinstance(item, Rescue):
+            raise TypeError
+        else:
+            for rescue in self._rescues.values():
+                if rescue == item:
+                    return True
+            return False
+
     def find_by_index(self, index: int) -> Rescue or None:
         """
         Searches for and returns a Rescue at a given `index` position, should it exist
