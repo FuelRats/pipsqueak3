45bfbc9584ee8b2caf5cb0f55e6497cdbbee4a18
MHajoha <MHajoha@gmail.com>
2018-04-22 23:44:43 +0200

    Adapt tests to changes

--- tests/test_trigger.py
+++ tests/test_trigger.py
@@ -9,16 +9,22 @@ def bot():
     return MockBot()
 
 def test_create_manual_channel(bot):
-    trigger = Trigger(bot, "test_nick", "#somechannel", "test_ident", "test.vhost")
+    trigger = Trigger(bot, ["don't", "care"], ["don't care", "care"], "test_nick", "#somechannel",
+                      "test_ident", "test.vhost")
     assert trigger.channel == "#somechannel"
 
 def test_create_manual_query(bot):
-    trigger = Trigger(bot, "test_nick", "not_a_channel", "test_ident", "test.vhost")
+    trigger = Trigger(bot, ["don't", "care"], ["don't care", "care"], "test_nick", "not_a_channel",
+                      "test_ident", "test.vhost")
     assert trigger.channel is None
 
 def test_create_from_user_channel(bot):
-    trigger = Trigger.from_bot_user(bot, "unit_test", "#somechannel")
+    trigger = Trigger.from_bot_user(bot, "unit_test", "#somechannel",
+                                    ["some", "thing"], ["some thing", "thing"])
     assert trigger.channel == "#somechannel"
+
+    assert trigger.words == ["some", "thing"]
+    assert trigger.words_eol == ["some thing", "thing"]
 
     assert trigger.nickname == bot.users["unit_test"]["nickname"]
     assert trigger.ident == bot.users["unit_test"]["username"]
@@ -29,8 +35,12 @@ def test_create_from_user_channel(bot):
     assert trigger.identified == bot.users["unit_test"]["identified"]
 
 def test_create_from_user_query(bot):
-    trigger = Trigger.from_bot_user(bot, "unit_test[BOT]", "not_a_channel")
+    trigger = Trigger.from_bot_user(bot, "unit_test[BOT]", "not_a_channel",
+                                    ["some", "thing"], ["some thing", "thing"])
     assert trigger.channel is None
+
+    assert trigger.words == ["some", "thing"]
+    assert trigger.words_eol == ["some thing", "thing"]
 
     assert trigger.nickname == bot.users["unit_test[BOT]"]["nickname"]
     assert trigger.ident == bot.users["unit_test[BOT]"]["username"]
@@ -42,7 +52,8 @@ def test_create_from_user_query(bot):
 
 @async_test
 async def test_reply_channel(bot):
-    trigger = Trigger(bot, "test_nick", "#somechannel", "test_ident", "test.vhost")
+    trigger = Trigger(bot, ["some", "thing"], ["some thing", "thing"],
+                      "test_nick", "#somechannel", "test_ident", "test.vhost")
     await trigger.reply("Exceedingly smart test message.")
     assert {
         "target": "#somechannel",
@@ -51,7 +62,8 @@ async def test_reply_channel(bot):
 
 @async_test
 async def test_reply_query(bot):
-    trigger = Trigger(bot, "test_nick", "not_a_channel", "test_ident", "test.vhost")
+    trigger = Trigger(bot, ["some", "thing"], ["some thing", "thing"],
+                      "test_nick", "not_a_channel", "test_ident", "test.vhost")
     await trigger.reply("Exceedingly smart test message.")
     assert {
         "target": "test_nick",
