e4fc45d8393f24d7ab52eefda41bfa4d88d55628
beepbeat <beepbeat@users.noreply.github.com>
2018-05-08 18:47:11 +0200

    Update DBM

--- Modules/database_manager.py
+++ Modules/database_manager.py
@@ -10,24 +10,19 @@ See LICENSE.md
 
 This module is built on top of the Pydle system.
 """
-import sqlite3
+import aiosqlite
+import config
 
 
 class DataBaseManager:
-    instance = None
+    __instance = None
 
-    def __init__(self, file_name: str = "mecha3.db"):
+    def __init__(self, file_name: str = config.DataBaseManager.default_file_name):
         """
         Creates the master instance of the nested singleton class
         """
-        if not self.instance:
-            self.instance = self.__DatabaseManager(file_name)
-
-#    def __getattr__(self, item):
-#        """
-#        Redirects the getattr calls to the singleton class
-#        """
-#        return getattr(self.instance, item)
+        if not self.__instance:
+            self.__instance = self.__DatabaseManager(file_name)
 
     class __DatabaseManager:
         """
@@ -40,11 +35,11 @@ class DataBaseManager:
             Initializes the connection and creates the default tables, should they not exist
             """
             self.filePath = file_name
-            self.connection = sqlite3.connect(self.filePath)
+            self.connection = aiosqlite.connect(self.filePath)
             if self._has_table("mecha3-facts"):
                 self._create_table("mecha3-facts", {"fact": "STRING", "lang": "STRING", "text": "STRING"})
 
-        def _has_table(self, name: str):
+        async def _has_table(self, name: str):
             """
 
             Args:
@@ -53,11 +48,16 @@ class DataBaseManager:
             Returns: True when table exists, false otherwise
 
             """
-            return len(self.connection.execute(
+            result = None
+            async with aiosqlite.connect(self.filePath) as db:
+                cursor = await db.execute(
                     "SELECT name FROM sqlite_master WHERE type='table' AND name=?", (name,))
-                .fetchall()) >= 1
+                result = await cursor.fetchone()
+                await cursor.close()
+            return result is not None
 
-        def _create_table(self, name: str, types: dict):
+
+        async def _create_table(self, name: str, types: dict):
             """
             Creates the table with the given name and columns
             Args:
@@ -67,16 +67,18 @@ class DataBaseManager:
 
             Returns: None
             """
-            if self._has_table(name):
+            if await self._has_table(name):
                 raise ValueError(f"Table {name} already exists")
             type_string: str = ""
             for k, v in types.items():
                 type_string += f" {k} {v.upper()},"
             type_string = type_string[:-1]
-            self.connection.execute("CREATE TABLE {name} ({t_string})".format(name=name,
-                                                                              t_string=type_string))
 
-        def _insert_row(self, table_name: str, values: tuple):
+            async with aiosqlite.connect(self.filePath) as db:
+                await db.execute(f"CREATE TABLE {name} ({type_string})")
+                await db.commit()
+
+        async def _insert_row(self, table_name: str, values: tuple):
             """
             Inserts the given row into the given table
             Args:
@@ -88,21 +90,19 @@ class DataBaseManager:
 
             Raises:
                 sqlite3.OperationalError: When the statement was malformed, most likely because
-                 #  the length of the tuple wasn't equal to the number of columns
+                  the length of the tuple wasn't equal to the number of columns
                 ValueError: When the given table does not exist
             """
-            if not self._has_table(table_name):
+            if not await self._has_table(table_name):
                 raise ValueError(f"Table {table_name} does not exist")
 
-            value_string: str = ""
-            for k in values:
-                value_string += f"{k},"
-            value_string = value_string[:-1]
+            value_string: str = ", ".join(values)
 
-            self.connection.execute("INSERT INTO '{tablename}' VALUES (?)".format(tablename=table_name),
-                                    (value_string,))
+            async with aiosqlite.connect(self.filePath) as db:
+                await db.execute(f"INSERT INTO '{table_name}' VALUES (?)", (value_string,))
+                await db.commit()
 
-        def _select_rows(self, table_name: str, connector: str, condition=None) -> list:
+        async def _select_rows(self, table_name: str, connector: str, condition: dict = None) -> list:
             """
             Return the rows from the specified table and filters them by the specified conditions.
                 Supports 'AND' and 'OR'
@@ -110,15 +110,16 @@ class DataBaseManager:
                 table_name (str): name of table to select from
                 connector (str): either 'AND' or 'OR'
                 condition(dict): a dict of conditions to filter the result.
-                    these will be connected via 'connector' and in the end must be true for the given row
-                        to be included in the result
+                    these will be connected via 'connector' and in the end
+                        must be true for the given row
+                            to be included in the result
             Returns: list[tuple]
                 list of tuples, where each tuple represents the row,
                     and each element of the tuple represents the value of the column.
             """
             if condition is None:
                 condition = {}
-            if not self._has_table(table_name):
+            if not await self._has_table(table_name):
                 raise ValueError(f"Table {table_name} does not exist")
 
             connector = connector.upper()
@@ -129,15 +130,23 @@ class DataBaseManager:
             for k, v in condition.items():
                 condition_string += f" {k}='{v}' {connector}"
             if len(condition) > 0:
-                condition_string = condition_string[:-len(connector)]
-                condition_string = f" WHERE {condition_string}"
+                condition_string = condition_string[1:-len(connector)]
 
-            return self.connection.execute("SELECT * FROM '{tablename}' {cond_str}".
-                                           format(tablename=table_name, cond_str=condition_string)).fetchall()
+            print(f"SELECT * FROM '{table_name}' WHERE {condition_string}")
 
-        def _update_row(self, table_name: str, connector: str, values: dict, condition=None):
+            async with aiosqlite.connect(self.filePath) as db:
+                cursor = await db.execute(
+                    f"SELECT * FROM {table_name} WHERE ?", (condition_string,)
+                )
+                await db.commit()
+                result = await cursor.fetchall()
+                await cursor.close()
+                return list(result)
+
+        async def _update_row(self, table_name: str, connector: str, values: dict, condition=None):
             """
-            Updates the rows of the given table with the given values filtering with the given conditions.
+            Updates the rows of the given table with the given values
+                filtering with the given conditions.
             Args:
                 table_name (str): name of table to update
                 connector (str): 'AND' or 'OR'
@@ -153,7 +162,7 @@ class DataBaseManager:
             """
             if condition is None:
                 condition = {}
-            if not self._has_table(table_name):
+            if not await self._has_table(table_name):
                 raise ValueError(f"Table {table_name} does not exist")
 
             if len(condition) <= 0:
@@ -167,17 +176,21 @@ class DataBaseManager:
             for k, v in condition.items():
                 condition_string += f" {k}='{v}' {connector}"
             if len(condition_string) > 0:
-                condition_string = condition_string[:-len(connector)]
+                condition_string = condition_string[1:-len(connector)]
 
             value_string: str = ""
             for k, v in values.items():
                 value_string += f" {k}='{v}',"
 
-            value_string = value_string[:-1]
+            value_string = value_string[1:-1]
 
-            self.connection.execute(f"UPDATE '{table_name}' SET {value_string} WHERE {condition_string}")
+            async with aiosqlite.connect(self.filePath) as db:
+                print("update string = " + f"UPDATE '{table_name}' SET {value_string} WHERE {condition_string}")
+                cursor = await db.execute(f"UPDATE '{table_name}' SET {value_string} WHERE ?", (condition_string,))
+                await db.commit()
+                await cursor.close()
 
-        def _delete_row(self, table_name: str, connector: str, condition=None):
+        async def _delete_row(self, table_name: str, connector: str, condition=None):
             """
             deletes the rows from the specified table and filters them by the specified conditions.
                 Supports 'AND' and 'OR'
@@ -197,7 +210,7 @@ class DataBaseManager:
             """
             if condition is None:
                 condition = {}
-            if not self._has_table(table_name):
+            if not await self._has_table(table_name):
                 raise ValueError(f"Table {table_name} does not exist")
 
             if not len(condition) > 0:
@@ -212,21 +225,31 @@ class DataBaseManager:
                 condition_string += f"{k}='{v}' {connector} "
             condition_string = condition_string[:-(len(connector) + 1)]
 
-            self.connection.execute(f"DELETE FROM '{table_name}' WHERE {condition_string}")
+            async with aiosqlite.connect(self.filePath) as db:
+                await db.execute(f"DELETE FROM '{table_name}' WHERE ?", (condition_string,))
+                await db.commit()
 
-    def get_fact(self, name: str, preferred_lang: str):
+    async def get_fact(self, name: str, preferred_lang: str) -> str:
         """
         Return the fact for the given fact name and language.
         If the preferred_language doesnt exist, it returns the fact for the english language.
         If neither exist, it returns an empty string
         Args:
-            name(str): name of the fact
-            preferred_lang (str): preferred language
+            name:
+             str: name of the fact
+            preferred_lang:
+             str: preferred language
 
-        Returns(str): fact for the given language, the english language or an empty string, if neither exist.
+        Returns:
+            str: fact for the given language, the english language or
+                an empty string, if neither exist.
 
         """
-        result = self.instance._select_rows("mecha3-facts", "AND", {"lang": preferred_lang, "fact":name})
+        result = await self.__instance._select_rows(
+            "mecha3-facts", "AND", {"lang": preferred_lang, "fact": name}
+        )
         if len(result) <= 0:
-            result = self.instance._select_rows("mecha3-facts", "AND", {"lang": "en", "fact":name})
+            result = await self.__instance._select_rows(
+                "mecha3-facts", "AND", {"lang": "en", "fact": name}
+            )
         return result[0][2] if len(result) > 0 else ""
