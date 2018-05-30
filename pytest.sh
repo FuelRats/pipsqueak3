74236db5587eb63c026bb81bdded0052b30c6f6a
beepbeat <beepbeat@users.noreply.github.com>
2018-05-31 01:25:43 +0200

    Update pytest.sh

--- pytest.sh
+++ pytest.sh
@@ -1,4 +1,4 @@
-PGPASSWORD=postgres psql -c 'CREATE ROLE mecha3 LOGIN CREATEDB ENCRYPTED PASSWORD mecha3' -U postgres -h localhost
+PGPASSWORD=postgres psql -c 'CREATE ROLE mecha3 LOGIN CREATEDB ENCRYPTED PASSWORD \'mecha3\'' -U postgres -h localhost
 PGPASSWORD=postgres psql -c 'CREATE DATABASE mecha3 WITH OWNER = mecha3' -U postgres -h localhost
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 chmod +x ./cc-test-reporter
