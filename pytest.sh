45ceca9ac260ec54ec90c3ec05a1c6cba67b05bc
holladiewal <holladiewal@whydoyouhate.me>
2018-06-03 03:06:38 +0200

    fixed relative path for config

--- pytest.sh
+++ pytest.sh
@@ -1,3 +1,3 @@
 psql -c 'CREATE ROLE mecha3 LOGIN CREATEDB' -U postgres -h $P_HOST
 psql -c 'CREATE DATABASE mecha3 WITH OWNER = mecha3' -U postgres -h $P_HOST
-pytest -v --cov --cov-report xml --junit-xml=test-reports --config ./config/testing.json
+pytest -v --cov --cov-report xml --junit-xml=test-reports --config ./testing.json
