c648e985981ec77ac45395334e3a89cdfb1654a3
theunkn0wn1 <thHunkn0WNd@gmail.com>
2018-06-03 21:37:01 -0700

    Added a User object (WIP)  - also added some debug commands

---
+++ Modules/User.py
@@ -0,0 +1,132 @@
+"""
+User.py - Provides a user object
+
+Provides an User object that represents a IRC User
+
+Copyright (c) 2018 The Fuel Rats Mischief,
+All rights reserved.
+
+Licensed under the BSD 3-Clause License.
+
+See LICENSE.md
+"""
+from typing import Union
+
+
+class User(object):
+    """
+    Represents an IRC user
+    """
+
+    def __init__(self,
+                 oper: bool,
+                 idle: int,
+                 away: bool,
+                 away_message: str,
+                 identified: bool,
+                 secure: bool,
+                 account: str,
+                 # attributes below this line are nullable
+                 username: Union[None, str] = None,
+                 hostname: Union[None, str] = None,
+                 realname: Union[None, str] = None,
+                 server: Union[None, str] = None,
+                 server_info: Union[None, str] = None):
+        """
+        Creates a new IRC user object
+        Args:
+            oper (bool): is the user an oper
+            idle (int): time user is idle for
+            away (bool): user's away flag
+            away_message (str): user's away message
+            username (str): username
+            hostname (str): hostname
+            realname (str): realname
+            identified (bool): NS identification flag
+            server (str): server the user belongs to
+            server_info (str): server's information string
+            secure (bool): Secure connection marker
+            account (str): NS identity
+
+        As a whois entry will not return some fields for users that don't exist,
+        Username, hostname, realname, server, and server_info are nullable.
+
+        """
+        self._oper: bool = oper
+        self._idle: int = idle
+        self._away: bool = away
+        self._away_message: str = away_message
+        self._username: str = username
+        self._hostname: str = hostname
+        self._realname: str = realname
+        self._identified: bool = identified
+        self._server: str = server
+        self._server_info: str = server_info
+        self._secure: bool = secure
+        self._account: str = account
+
+    @property
+    def oper(self) -> bool:
+        """User's OPER flag, False if they are not an Oper"""
+        return self._oper
+
+    @property
+    def idle(self) -> int:
+        """Idle timer"""
+        return self._idle
+
+    @property
+    def away(self) -> bool:
+        """User's away flag"""
+        return self._away
+
+    @property
+    def away_message(self) -> Union[None, str]:
+        """
+        User's away message, if it exists. otherwise None
+        """
+        return self._away_message
+
+    @property
+    def username(self) -> str:
+        """Username"""
+        return self._username
+
+    @property
+    def hostname(self) -> str:
+        """Hostname"""
+        return self._hostname
+
+    @property
+    def realname(self) -> str:
+        """User's set realname"""
+        return self._realname
+
+    @property
+    def identified(self) -> bool:
+        """Nickserv ident flag"""
+        return self._identified
+
+    @property
+    def server(self) -> str:
+        """
+        Server user exists on
+
+        (this would be the server the WHOIS was run against)
+        """
+        return self._server
+
+    @property
+    def server_info(self) -> str:
+        """Server information"""
+        return self._server_info
+
+    @property
+    def secure(self) -> bool:
+        """Secure connection flag"""
+        return self._secure
+
+    @property
+    def account(self) -> Union[str, None]:
+        """Nickserv account, if applicable"""
+        return self._account
---
+++ commands/debug.py
@@ -0,0 +1,27 @@
+"""
+debug.py - Debug and diagnostics commands
+
+Provides IRC commands geared towards debugging mechasqueak itself.
+This module should **NOT** be loaded in a production environment
+
+Copyright (c) 2018 The Fuel Rats Mischief,
+All rights reserved.
+
+Licensed under the BSD 3-Clause License.
+
+See LICENSE.md
+"""
+import logging
+
+from Modules.permissions import require_permission, TECHRAT
+from Modules.rat_command import Commands
+
+log = logging.getLogger(f"mecha.{__name__}")
+
+
+@require_permission(TECHRAT)
+@Commands.command("debug-whois")
+async def cmd_debug_whois(bot, trigger):
+    data = await bot.whois(trigger.words[1])
+    log.debug(data)
+    await trigger.reply("done.")
