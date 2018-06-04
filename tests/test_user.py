31de5d8938be248f690ff9640c9fb6322aa4764b
theunkn0wn1 <thHunkn0WNd@gmail.com>
2018-06-04 16:59:08 -0700

    Added a test for the User constructor

--- tests/test_user.py
+++ tests/test_user.py
@@ -38,3 +38,62 @@ def test_process_vhost_orange():
     (that can't be tested in the parametrize)
     """
     assert User.process_vhost("i.see.all") == "i.see.all"
+
+
+@pytest.mark.parametrize("data", (
+        {'oper': False,
+         'idle': 0,
+         'away': False,
+         'away_message': None,
+         'username': 'White',
+         'hostname': 'recruit.fuelrats.com',
+         'realname': 'WhiteStrips',
+         'identified': False,
+         'server': 'irc.fuelrats.com',
+         'server_info': 'Fuel Rats IRC Server',
+         'secure': True,
+         'account': 'WhiteStrips'},
+        {'oper': True,
+         'idle': 0,
+         'away': False,
+         'away_message': None,
+         'username': 'AwesomeAdmin',
+         'hostname': 'admin.fuelrats.com',
+         'realname': 'you know',
+         'identified': True,
+         'server': 'irc.fuelrats.com',
+         'server_info': 'Fuel Rats IRC Server',
+         'secure': True,
+         'account': 'AwesomeAdmin'}
+))
+def test_user_constructor(data: dict):
+    """
+    Tests the User constructor
+    """
+    my_user = User(oper=data['oper'],
+                   idle=data['idle'],
+                   away=data['away'],
+                   away_message=data['away_message'],
+                   identified=data['identified'],
+                   secure=data['secure'],
+                   account=data['account'],
+                   nickname="unit_test",
+                   username=data['username'],
+                   hostname=data['hostname'],
+                   realname=data['realname'],
+                   server=data['server'],
+                   server_info=data['server_info'],
+                   )
+
+    assert data['oper'] == my_user.oper
+    assert data['idle'] == my_user.idle
+    assert data['away'] == my_user.away
+    assert data['away_message'] == my_user.away_message
+    assert data['identified'] == my_user.identified
+    assert data['secure'] == my_user.secure
+    assert data['account'] == my_user.account
+    assert "unit_test" == my_user.nickname
+    assert data['hostname'] == my_user.hostname
+    assert data['realname'] == my_user.realname
+    assert data['server'] == my_user.server
+    assert data['server_info'] == my_user.server_info
