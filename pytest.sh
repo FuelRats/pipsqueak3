074d5f298a48e77c3c44c0dcc27dee8ef4363e90
beepbeat <beepbeat@users.noreply.github.com>
2018-06-22 21:27:06 +0200

    Update pytest.sh

--- pytest.sh
+++ pytest.sh
@@ -1,5 +1,5 @@
 export P_HOST
 psql -c 'CREATE ROLE mecha3 LOGIN CREATEDB' -U postgres -h $P_HOST
 psql -c 'CREATE DATABASE mecha3 WITH OWNER = mecha3' -U postgres -h $P_HOST
-pytest -v --cov --cov-report xml --junit-xml=test-reports/junit.xml --config ./testing.json
+pytest -v --cov --cov-report xml --junit-xml=test-reports/junit.xml
 cp coverage.xml /test-data/coverage.xml
