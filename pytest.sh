61f97c9bbc9bb96d8b96d620454b3cd7cd43e3f5
beepbeat <beepbeat@users.noreply.github.com>
2018-06-25 03:22:15 +0200

    Update pytest.sh

--- pytest.sh
+++ pytest.sh
@@ -2,6 +2,6 @@ export P_HOST
 psql -c 'CREATE ROLE mecha3 LOGIN CREATEDB' -U postgres -h $P_HOST
 psql -c 'CREATE DATABASE mecha3 WITH OWNER = mecha3' -U postgres -h $P_HOST
 pytest -v --cov --cov-report xml --junit-xml=test-reports/junit.xml
-exitcode = $?
-if [ $exitcode -ne 0 ] then exit 1
+exitcode=$?
+if [ $exitcode -ne 0 ] then exit 1 fi
 cp coverage.xml /test-data/coverage.xml
