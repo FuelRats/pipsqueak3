6804fcd3233dc6487da1e2a1def3792780aad39f
holladiewal <holladiewal@whydoyouhate.me>
2018-06-25 23:06:33 +0200

    Adding work already done on the DBM

--- utils/ratlib.py
+++ utils/ratlib.py
@@ -97,3 +97,12 @@ def try_parse_uuid(suspect: str) -> UUID:
 
     else:
         return result
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
