a46d3116793e00835ccb0bff3a3bafb91398ea3a
theunkn0wn1 <thHunkn0WNd@gmail.com>
2018-06-25 14:05:59 -0700

    SPARK-13 Renamed Rats into Rat, as to reflect its function  - preparation to migrate the rat cache functions to rat_cache

--- tests/test_quotes.py
+++ tests/test_quotes.py
@@ -1,7 +1,7 @@
 """
 rat_rescue.py - Rescue board and objects
 
-Copyright (c) 2018 The Fuel Rats Mischief,
+Copyright (c) 2018 The Fuel Rat Mischief,
 All rights reserved.
 
 Licensed under the BSD 3-Clause License.
