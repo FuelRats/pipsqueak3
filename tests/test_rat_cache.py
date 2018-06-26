8e8c7004968f6b8dfae409b863c67041d3bcff99
theunkn0wn1 <thHunkn0WNd@gmail.com>
2018-06-25 20:12:38 -0700

    SPARK-13 Moar tests

--- tests/test_rat_cache.py
+++ tests/test_rat_cache.py
@@ -8,9 +8,12 @@ Licensed under the BSD 3-Clause License.
 
 See LICENSE.md
 """
+from uuid import uuid4
+
 import pytest
 
 from Modules.rat import Rat
+from Modules.rat_cache import RatCache
 from utils.ratlib import Platforms
 
 pytestmark = pytest.mark.asyncio
@@ -50,3 +53,37 @@ async def test_find_rat_by_name_not_existing(rat_cache_fx):
 
     found = await rat_cache_fx.get_rat_by_name(name="chapstix", platform=Platforms.XB)
     assert found is None
+
+
+async def test_by_uuid_setter_valid(rat_cache_fx: RatCache, rat_good_fx: Rat):
+    fdict = {rat_good_fx.uuid: rat_good_fx}
+    rat_cache_fx.by_uuid = fdict
+    assert fdict == rat_cache_fx.by_uuid
+
+
+@pytest.mark.parametrize("garbage", [None, -1, 2.42, "pink salad", []])
+async def test_by_uuid_setter_invalid(garbage, rat_cache_fx):
+    with pytest.raises(TypeError):
+        rat_cache_fx.by_uuid = garbage
+
+
+async def test_by_name_setter_valid(rat_cache_fx, rat_good_fx: Rat):
+    fdict = {rat_good_fx.name: rat_good_fx}
+
+    rat_cache_fx.by_name = fdict
+    assert fdict == rat_cache_fx.by_name
+
+
+@pytest.mark.parametrize("garbage", [None, -1, 2.42, "pink salad", []])
+async def test_by_name_setter_garbage(rat_cache_fx: RatCache, garbage):
+    with pytest.raises(TypeError):
+        rat_cache_fx.by_name = garbage
+
+
+async def test_flush(rat_cache_fx: RatCache):
+    rat_cache_fx.by_name = {"s": 12}
+    rat_cache_fx.by_uuid = {uuid4(): 12}
+
+    rat_cache_fx.flush()
+    assert {} == rat_cache_fx.by_uuid
+    assert {} == rat_cache_fx.by_name
