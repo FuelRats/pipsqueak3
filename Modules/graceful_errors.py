744cd14894a28b6811554945740488722b207736
theunkn0wn1 <thHunkn0WNd@gmail.com>
2018-06-29 15:15:34 -0700

    SPARK-24 Add a `graceful_errors` module and `make_graceful` function  - used to pretty-print exceptions presented to users

--- Modules/graceful_errors.py
+++ Modules/graceful_errors.py
@@ -12,8 +12,34 @@ Licensed under the BSD 3-Clause License.
 See LICENSE.md
 """
 from typing import Dict
+from uuid import UUID
 
 by_error: Dict[type(Exception), str] = {
-    ZeroDivisionError: "Rotten.cheddar!",
-    TypeError: "Cheddar.rottenCheese! ",
+    ZeroDivisionError: "Rotten cheddar",
+    TypeError: "Bad Tangerines",
 }
+
+
+def make_graceful(ex: Exception, ex_uuid: UUID) -> str:
+    """
+    makes the incoming exception graceful
+
+    Args:
+        ex (Exception): the raised exception to make graceful
+
+    Returns:
+        str: graceful error message
+    """
+
+    base = "Oh noes! {message} encountered! please contact a tech! Reference code {uuid}"
+
+    ex_type = type(ex)
+
+    # gets the first octet from the uuid
+    octet = str(ex_uuid)[:8]
+
+    message = by_error[ex_type] if ex_type in by_error else "unknown error"
+
+    output = base.format(message=message, uuid=octet)
+
+    return output
