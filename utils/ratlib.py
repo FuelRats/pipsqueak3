702334abbc9b74cbddeba25e612f6acc1a945f71
holladiewal <holladiewal@whydoyouhate.me>
2018-07-12 22:11:07 +0200

    Incorporating the new Singleton as well as other new changes removing the abstract

--- utils/ratlib.py
+++ utils/ratlib.py
@@ -128,12 +128,3 @@ def try_parse_uuid(suspect: str) -> UUID:
 
     else:
         return result
-
-
-class Singleton(type):
-    _instance = None
-
-    def __call__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instance
