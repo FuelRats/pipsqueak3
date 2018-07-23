6e5beedded175df77e4394fbb80fc27698ab8c95
Shatt <shatteredbeam@gmail.com>
2018-07-23 16:58:28 -0700

    docstring updates

--- tests/conftest.py
+++ tests/conftest.py
@@ -182,7 +182,8 @@ def logging_fx(caplog) -> logging.Logger:
 @pytest.fixture
 def spooled_logging_fx(caplog) -> logging.Logger:
     """
-    Calls the logging engine with a spooled file for testing strings only.
+    Same as logging_fx, but under separate namespace for the logger to
+    use a temporary file.
     """
     caplog.clear()
     return logging.getLogger("mecha.spooled_logging_fx")
