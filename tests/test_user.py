e20b4c998cf34572dac73420da9d4e052caec86f
theunkn0wn1 <thHunkn0WNd@gmail.com>
2018-11-01 22:41:49 -0700

    Removed dependency on whois for user objects.
    Pydle had a builtin method for doing this stuff.
    
    This removes `User.from_whois` and replaces it with `User.from_pydle`

--- tests/test_user.py
+++ tests/test_user.py
@@ -11,7 +11,9 @@ See LICENSE.md
 import pytest
 
 from Modules.user import User
+
 pytestmark = pytest.mark.user
+
 
 @pytest.mark.parametrize("expected_host", [
     "recruit.fuelrats.com",
@@ -50,63 +52,47 @@ def test_process_vhost_garbage(garbage: str):
 
 
 @pytest.mark.parametrize("data", (
-        {'oper': False,
-         'idle': 0,
-         'away': False,
-         'away_message': None,
-         'username': 'White',
-         'hostname': 'recruit.fuelrats.com',
-         'realname': 'WhiteStrips',
-         'identified': False,
-         'server': 'irc.fuelrats.com',
-         'server_info': 'Fuel Rat IRC Server',
-         'secure': True,
-         'account': 'WhiteStrips'},
-        {'oper': True,
-         'idle': 0,
-         'away': False,
-         'away_message': None,
-         'username': 'AwesomeAdmin',
-         'hostname': 'admin.fuelrats.com',
-         'realname': 'you know',
-         'identified': True,
-         'server': 'irc.fuelrats.com',
-         'server_info': 'Fuel Rat IRC Server',
-         'secure': True,
-         'account': 'AwesomeAdmin'}
+        {
+            'away': False,
+            'away_message': None,
+            'username': 'White',
+            'hostname': 'recruit.fuelrats.com',
+            'realname': 'WhiteStrips',
+            'identified': False,
+
+            'account': 'WhiteStrips'},
+        {
+            'away': False,
+            'away_message': None,
+            'username': 'AwesomeAdmin',
+            'hostname': 'admin.fuelrats.com',
+            'realname': 'you know',
+            'identified': True,
+            'account': 'AwesomeAdmin'}
 ))
 def test_user_constructor(data: dict):
     """
     Tests the User constructor
     """
-    my_user = User(oper=data['oper'],
-                   idle=data['idle'],
-                   away=data['away'],
-                   away_message=data['away_message'],
-                   identified=data['identified'],
-                   secure=data['secure'],
-                   account=data['account'],
-                   nickname="unit_test",
-                   username=data['username'],
-                   hostname=data['hostname'],
-                   realname=data['realname'],
-                   server=data['server'],
-                   server_info=data['server_info'],
-                   )
+    my_user = User(
+        away=data['away'],
+        away_message=data['away_message'],
+        identified=data['identified'],
+        account=data['account'],
+        nickname="unit_test",
+        username=data['username'],
+        hostname=data['hostname'],
+        realname=data['realname'],
+    )
 
-    assert data['oper'] == my_user.oper
-    assert data['idle'] == my_user.idle
     assert data['away'] == my_user.away
     assert data['away_message'] == my_user.away_message
     assert data['identified'] == my_user.identified
-    assert data['secure'] == my_user.secure
     assert data['account'] == my_user.account
     assert "unit_test" == my_user.nickname
     assert data['hostname'] == my_user.hostname
     assert data['username'] == my_user.username
     assert data['realname'] == my_user.realname
-    assert data['server'] == my_user.server
-    assert data['server_info'] == my_user.server_info
 
 
 @pytest.mark.asyncio
@@ -115,22 +101,18 @@ async def test_user_from_whois_existing_user(bot_fx):
     verifies building a User from a full IRC reply when said user exists
     """
 
-    my_user = await User.from_whois(bot_fx, "some_recruit")
+    my_user = await User.from_pydle(bot_fx, "some_recruit")
 
     data = bot_fx.users['some_recruit']
-    assert data['oper'] == my_user.oper
-    assert data['idle'] == my_user.idle
+
     assert data['away'] == my_user.away
     assert data['away_message'] == my_user.away_message
     assert data['identified'] == my_user.identified
-    assert data['secure'] == my_user.secure
     assert data['account'] == my_user.account
     assert "some_recruit" == my_user.nickname
     assert data['hostname'] == my_user.hostname
     assert data['username'] == my_user.username
     assert data['realname'] == my_user.realname
-    assert data['server'] == my_user.server
-    assert data['server_info'] == my_user.server_info
 
 
 @pytest.mark.asyncio
@@ -141,71 +123,40 @@ async def test_user_from_whois_miss(monkeypatch, bot_fx):
     # patch in the exception raiser
     monkeypatch.setattr("tests.mock_bot.MockBot.whois", mock_whois)
 
-    ret = await User.from_whois(bot_fx, "snafu")
+    ret = await User.from_pydle(bot_fx, "snafu")
     assert ret is None
 
 
 @pytest.mark.asyncio
-async def test_user_from_whois_malformed_return(monkeypatch, bot_fx):
-    async def mock_return(*args) -> dict:
-        """returns a malformed dict"""
-        return {}
-
-    monkeypatch.setattr("tests.mock_bot.MockBot.whois", mock_return)
-    with pytest.raises(ValueError):
-        await User.from_whois(bot_fx, "FUBAR")
-
-
-@pytest.mark.asyncio
 @pytest.mark.parametrize("data", (
-        {'oper': False,
-         'idle': 0,
-         'away': False,
-         'away_message': None,
-         'username': 'White',
-         'hostname': 'recruit.fuelrats.com',
-         'realname': 'WhiteStrips',
-         'identified': False,
-         'server': 'irc.fuelrats.com',
-         'server_info': 'Fuel Rat IRC Server',
-         'secure': True,
-         'account': 'WhiteStrips'},
-        {'oper': True,
-         'idle': 0,
-         'away': False,
-         'away_message': None,
-         'username': 'AwesomeAdmin',
-         'hostname': 'admin.fuelrats.com',
-         'realname': 'you know',
-         'identified': True,
-         'server': 'irc.fuelrats.com',
-         'server_info': 'Fuel Rat IRC Server',
-         'secure': True,
-         'account': 'AwesomeAdmin'}
+        {
+            'nickname': "Whitestrip",
+            'away': False,
+            'away_message': None,
+            'username': 'White',
+            'hostname': 'recruit.fuelrats.com',
+            'realname': 'WhiteStrips',
+            'identified': False,
+
+            'account': 'WhiteStrips'},
+        {
+            'nickname': "CaptainAwesome",
+            'away': False,
+            'away_message': None,
+            'username': 'AwesomeAdmin',
+            'hostname': 'admin.fuelrats.com',
+            'realname': 'you know',
+            'identified': True,
+            'account': 'AwesomeAdmin'}
 ))
 async def test_user_eq(data: dict, monkeypatch, bot_fx):
     """
     verifies building a User from a full IRC reply when said user exists
     """
 
-    async def mock_return(*args) -> dict:
-        return data
+    monkeypatch.setattr(bot_fx, 'users', {data['nickname'].casefold(): data})
 
-    monkeypatch.setattr("tests.mock_bot.MockBot.whois", mock_return)
-
-    user_alpha = await User.from_whois(bot_fx, "unit_test")
-    user_beta = User(data['oper'],
-                     data['idle'],
-                     data['away'],
-                     data['away_message'],
-                     data['identified'],
-                     data['secure'],
-                     data['account'],
-                     "unit_test",
-                     data['username'],
-                     data['hostname'],
-                     data['realname'],
-                     data['server'],
-                     data['server_info'])
+    user_alpha = await User.from_pydle(bot_fx, data['nickname'])
+    user_beta = User(**data)
 
     assert user_alpha == user_beta
