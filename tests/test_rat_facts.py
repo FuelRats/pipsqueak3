2514668fe0f1da8f468aeda61d43335624da790b
beepbeat <holladiewal@whydoyouhate.me>
2018-11-09 09:58:40 +0100

    I should really RTFM.

--- tests/test_rat_facts.py
+++ tests/test_rat_facts.py
@@ -118,7 +118,7 @@ class TestFacts(object):
         # checking it did not reply, or replied with an empty string
         assert not async_callable_fx.was_called
 
-        context = await Context.from_message(bot_fx, "#unit_test", "some_drillee", "!test1-en")
+        context = await Context.from_message(bot_fx, "#unit_test", "some_recruit", "!test1-en")
         async_callable_fx.reset()
         monkeypatch.setattr(context, "reply", async_callable_fx)
 
