ed8eb34e63284a6a9eb22c235439593fb53853da
Shatt <shatteredbeam@gmail.com>
2018-12-22 16:54:05 -0800

    New branch for PR.  Add Connection Pooling.  Create Package.

---
+++ database/__init__.py
@@ -0,0 +1,13 @@
+"""
+__init__.py
+
+Copyright (c) 2018 The Fuel Rat Mischief,
+All rights reserved.
+
+Licensed under the BSD 3-Clause License.
+
+See LICENSE.md
+"""
+__all__ = ["database_manager"]
+
+from .database_manager import DatabaseManager
