50732f9fbe79933d7aec476c46a966b8f429615b
Shatt <shatteredbeam@gmail.com>
2018-12-22 18:52:16 -0800

    Add connection pooling.  remove single quote from stripped chars in ratlib, build database_manager package.

--- database/database_manager.py
+++ database/database_manager.py
@@ -58,15 +58,37 @@ class DatabaseManager(Singleton, object):
         except (Exception, psycopg2.DatabaseError) as error:
             log.warning("Error connecting to SQL database.", error)
 
-    async def query(self, query, values: tuple) -> list:
+    async def query(self, query: sql.SQL, values: tuple) -> list:
+        """
+        Send a query to the connected database.  Pulls a connection from the pool and creates a cursor,
+        executing the composed query with the values.  Requires a composed SQL object (See psycopg2 docs)
+        Args:
+            query: composed SQL query object
+            values: tuple of values for query
+
+        Returns:
+            List of rows matching query.  May return an empty list if there are no matching rows.
+
+        """
+        # Verify composed SQL object
+        if not isinstance(query, sql.SQL):
+            raise TypeError("Expected composed SQL object for query.")
+
+        # Verify value is tuple
+        if not isinstance(values, tuple):
+            raise TypeError("Expected tuples for query values.")
+
+        # Pull a connection from the pool, and create a cursor from it.
         with self._dbpool.getconn() as connection:
+            # If we could set these at connection time, we would, but they must be set outside the pool.
             connection.autocommit = True
             connection.set_client_encoding('utf-8')
+            # Create cursor, and execute the query.
             with connection.cursor() as cursor:
                 cursor.execute(query, values)
                 result = cursor.fetchall()
 
+        # Release connection back to the pool.
         self._dbpool.putconn(connection)
 
         return list(result)
-
