2453a95d3c4eb4e4db571f15543ec34cd7665430
Shatt <shatteredbeam@gmail.com>
2018-12-24 20:36:33 -0800

    Refactor test_dbm and test_connecton_pool to include _fx suffix.

--- tests/test_database_manager.py
+++ tests/test_database_manager.py
@@ -32,77 +32,77 @@ def test_dbm_singleton():
     assert ship_crew is hating_sharks
 
 
-def test_init_values(test_dbm):
+def test_init_values(test_dbm_fx):
     """
     Test and verify all values required for instantiation are not None.
     """
-    assert test_dbm._initialized
-    assert test_dbm._dbhost
-    assert test_dbm._dbpass
-    assert test_dbm._dbname
-    assert test_dbm._dbport
-    assert test_dbm._dbuser
+    assert test_dbm_fx._initialized
+    assert test_dbm_fx._dbhost
+    assert test_dbm_fx._dbpass
+    assert test_dbm_fx._dbname
+    assert test_dbm_fx._dbport
+    assert test_dbm_fx._dbuser
 
 
-def test_db_connection(test_dbm_pool):
+def test_db_connection(test_dbm_pool_fx):
     """
     Pull a connection from the pool, and verify the status is able to receive a query.
     """
-    with test_dbm_pool.getconn() as connection:
+    with test_dbm_pool_fx.getconn() as connection:
         assert connection.status == psycopg2.extensions.STATUS_READY
 
 
-def test_db_connection_close(test_dbm_pool):
+def test_db_connection_close(test_dbm_pool_fx):
     """
     Assert that putting a connection back into the pool raises no error.
     """
-    conn = test_dbm_pool.getconn()
-    test_dbm_pool.putconn(conn, True)
+    conn = test_dbm_pool_fx.getconn()
+    test_dbm_pool_fx.putconn(conn, True)
 
 
-def test_db_connection_autocommit(test_dbm_pool):
+def test_db_connection_autocommit(test_dbm_pool_fx):
     """
     Verify autocommit is valid when set.
     """
-    with test_dbm_pool.getconn() as conn:
+    with test_dbm_pool_fx.getconn() as conn:
         conn.autocommit = True
         assert conn.autocommit
 
 
-def test_db_connection_client_encoding(test_dbm_pool):
+def test_db_connection_client_encoding(test_dbm_pool_fx):
     """
     Verify client encoding is valid when set.
     """
-    with test_dbm_pool.getconn() as conn:
+    with test_dbm_pool_fx.getconn() as conn:
         conn.set_client_encoding('utf-8')
         assert conn.encoding == 'UTF8'
 
 
 @pytest.mark.asyncio
-async def test_query_sql_error(test_dbm):
+async def test_query_sql_error(test_dbm_fx):
     """
     Verify a TypeError is raised if sending a string to query.
     """
     # Below statement is incorrect intentionally.
     with pytest.raises(TypeError) as error:
-        result = await test_dbm.query("This is a test", (15, 122))
+        result = await test_dbm_fx.query("This is a test", (15, 122))
 
 
 @pytest.mark.asyncio
-async def test_query_value_error(test_dbm):
+async def test_query_value_error(test_dbm_fx):
     """
     Verify a TypeError is raised if sending a non-tuple as query value.
     """
     # Below statement is incorrect intentionally.
     with pytest.raises(TypeError) as error:
-        result = await test_dbm.query(sql.SQL("SELECT * FROM {}", ["Happy", [1, 3, 4]]))
+        result = await test_dbm_fx.query(sql.SQL("SELECT * FROM {}", ["Happy", [1, 3, 4]]))
 
 
-def test_db_query_pool(test_dbm_pool):
+def test_db_query_pool(test_dbm_pool_fx):
     """
     Query the database using a connection pool.
     """
-    with test_dbm_pool.getconn().cursor() as cursor:
+    with test_dbm_pool_fx.getconn().cursor() as cursor:
         cursor.execute("SELECT name, lang ,message from fact WHERE name = 'test' AND lang='en'")
         result = cursor.fetchall()
 
@@ -110,10 +110,10 @@ def test_db_query_pool(test_dbm_pool):
 
 
 @pytest.mark.asyncio
-async def test_db_query_direct(test_dbm):
+async def test_db_query_direct(test_dbm_fx):
     """
     Query the database using the query function directly.
     """
-    assert await test_dbm.query(sql.SQL("SELECT name, lang, message "
+    assert await test_dbm_fx.query(sql.SQL("SELECT name, lang, message "
                                         "from fact WHERE name=%s AND lang=%s"),
-                                ('test', 'en')) == [('test', 'en', 'This is a test fact.')]
+                                   ('test', 'en')) == [('test', 'en', 'This is a test fact.')]
