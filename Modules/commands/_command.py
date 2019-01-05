3864cb07dc7b48465bfeb77b6a50d4296eea1c8e
theunkn0wn1 <thHunkn0WNd@gmail.com>
2019-01-05 11:28:22 -0800

    Fix teardown bug. added a bunch of logging to facilitate debugging

--- Modules/commands/_command.py
+++ Modules/commands/_command.py
@@ -10,7 +10,8 @@ Licensed under the BSD 3-Clause License.
 
 See LICENSE.md
 """
-from asyncio import gather
+from asyncio import run
+from collections import abc
 from logging import getLogger
 from typing import List, Callable
 
@@ -53,13 +54,13 @@ class Command:
             assert not require_dm, "require_dm and require_channel are mutually exclusive."
             self._setup_hooks.append(_hooks.require_channel)
 
-    def __contains__(self, item):
-        # check if the item is one of our aliases
-        if item in self.aliases:
-            return True
-        else:
-            # its not, bail out
+    def __contains__(self, item: str):
+        # check if its a string
+        if not isinstance(item, str):
+            # its not, bailout
             return NotImplemented
+        # check if the specified item is one of ours
+        return item in self.aliases
 
     @property
     def name(self):
@@ -132,16 +133,26 @@ class Command:
 
         LOG.debug(f"command {self.aliases[0]} invoked...")
         # call setup tasks sequentially
-        async for result in self.setup(*args, **kwargs):
-            LOG.debug(f"result of setup hook:= {result}")
-            # check if the hook wants to stop the execution
-            if result is _hooks.STOP_EXECUTION:
-                LOG.debug(f"Setup hook terminated execution for command {self.aliases[0]}")
-                return
 
-        LOG.debug(f"invoking the underlying for command '{self.aliases[0]}'")
+        if self.setup_hooks:
+            async for result in self.setup(*args, **kwargs):
+                LOG.debug(f"<{self.name}>:result of setup hook:= {result}")
+                # check if the hook wants to stop the execution
+                if result is _hooks.STOP_EXECUTION:
+                    LOG.debug(f"<{self.name}>:Setup hook terminated execution for command")
+                    return
+        else:
+            LOG.debug(f"<{self.name}>:no setup hooks for command, skipping...")
+
+        LOG.debug(f"<{self.name}>:invoking the underlying...")
         # all is good, invoke the underlying
         await self.underlying(*args, **kwargs)
 
         # finally execute teardown tasks some teardown
-        await gather(self.teardown(*args, **kwargs))
+
+        if self.teardown_hooks:
+            LOG.debug(f"<{self.name}>:executing teardown tasks for command")
+            async for task_result in self.teardown(*args, **kwargs):
+                LOG.debug(f"<{self.name}>: result of teardown hook: {task_result}")
+        else:
+            LOG.debug(f"<{self.name}>:no teardown hooks for command.")
