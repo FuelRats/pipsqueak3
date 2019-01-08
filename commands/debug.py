0e942890a3a08923b7064f6d339ebfbc218cf4a9
Shatt <shatteredbeam@gmail.com>
2019-01-08 13:22:06 -0800

    remove debug commands from debug.py

--- commands/debug.py
+++ commands/debug.py
@@ -16,9 +16,6 @@ import logging
 from Modules.context import Context
 from Modules.permissions import require_permission, TECHRAT, require_channel
 from Modules.rat_command import command
-from Modules.fact_manager import FactManager, Fact
-from database import DatabaseManager
-from psycopg2 import sql
 
 log = logging.getLogger(f"mecha.{__name__}")
 
@@ -51,43 +48,3 @@ async def cmd_debug_userinfo(context: Context):
 async def cmd_superping(context: Context):
     await context.reply("pong!")
 
-
-@command("debug-fact")
-@require_permission(TECHRAT)
-async def cmd_query(context: Context):
-    # assuming `database` is a instance of the DBM
-    database = DatabaseManager()
-    sql_query = sql.SQL("SELECT message from fact2 WHERE name=%s AND lang=%s")
-    sql_values = (context.words[1], context.words[2])
-    data = await database.query(sql_query, sql_values)
-    if not data:
-        await context.reply("No fact matched query.")
-    else:
-        for item in data:
-            await context.reply(str(item))
-
-
-@command("fm-find")
-@require_permission(TECHRAT)
-async def cmd_fm_find(context: Context):
-    fm = FactManager()
-    result = Fact()
-    result = await fm.find(context.words[1], context.words[2])
-
-    await context.reply(f"Name: {result.name}")
-    await context.reply(f"Message: {result.message}")
-    await context.reply(f"Author: {result.author}")
-
-
-@command("fm-add")
-@require_permission(TECHRAT)
-async def cmd_fm_add(context: Context):
-    new_fact = Fact(name='insert',
-                    lang='en',
-                    author='Shatt',
-                    message='This is a test fact inserted by debug command.',
-                    editedby='Shatt',
-                    mfd=False)
-    fm = FactManager()
-    await fm.add(new_fact)
-    await context.reply("Debug Fact Added.")
