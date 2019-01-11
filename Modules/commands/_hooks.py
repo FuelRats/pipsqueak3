05db0e5da44d2c9daffd2b8765efda21a704ec43
Orion (Theunkn0wn1) <thhunkn0wnd@gmail.com>
2019-01-10 18:32:39 -0800

    Begun writing a more formal Hook system

--- Modules/commands/_hooks.py
+++ Modules/commands/_hooks.py
@@ -10,9 +10,8 @@ Licensed under the BSD 3-Clause License.
 
 See LICENSE.md
 """
-from collections.abc import Callable
 from logging import getLogger
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Optional, Dict, Any, NoReturn
 
 from Modules.context import Context
 
@@ -25,6 +24,72 @@ class CancelExecution(Exception):
     **note** This should __only__ be raised by pre-execution hooks!
     """
     ...
+
+
+class HookImplementation:
+    __slots__ = ['__weakref__']  # disallow dynamic attribute creation (__dict__)
+
+    async def pre_execute(self, context: Context) -> Optional[Dict[str, Any]]:
+        """
+        Pre-execution Hook method. Anything you want to execute prior to running
+        the underlying goes in here.
+
+        To cancel the execution of underlying simply raise self.cancel(reason).
+
+        Args:
+            context (Context): Execution context
+
+        Returns:
+            Optional[Dict]: keyword arguments to pass to underling executable
+        """
+        ...
+
+    async def post_execute(self, context: Context) -> NoReturn:
+        """
+        Teardown tasks to execute.
+
+        This routine will be called even if the underlying was canceled by a setup routine.
+
+        Args:
+            context (Context): execution Cotnext
+        """
+        ...
+
+    Cancel = CancelExecution  # convenience alias
+
+    async def __call__(self, context: Context) -> AsyncGenerator:
+        # call our setup routine
+        yield await self.pre_execute(context)
+        await self.post_execute(context)
+
+
+class RequireChannelHook(HookImplementation):
+    """
+    Require Channel hook implementation
+    """
+    __slots__ = ['message']
+
+    def __init__(self, message="this MUST be executed in a channel"):
+        self.message = message
+
+    async def pre_execute(self, context: Context):
+        LOG.debug("in require_channel")
+        if not context.channel:
+            await context.reply(self.message)
+            raise self.Cancel(self.message)
+
+
+class RequireDirectMessageHook(HookImplementation):
+    __slots__ = ['message']
+
+    def __init__(self, message="this MUST be executed from a direct message with me!"):
+        self.message = message
+
+    async def pre_execute(self, context: Context):
+        LOG.debug("in RequireDirectMessage setup...")
+        if context.channel:
+            await context.reply(self.message)
+            raise self.Cancel(self.message)
 
 
 # #######
