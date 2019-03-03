81441673eb8f4fa059df93a51df40498e75aa30c
Shatt <shatteredbeam@gmail.com>
2019-03-02 21:20:56 -0800

    Re-add missing folders. Thanks pycharm.

---
+++ src/commands/debug.py
@@ -0,0 +1,50 @@
+"""
+debug.py - Debug and diagnostics commands
+
+Provides IRC commands geared towards debugging mechasqueak itself.
+This module should **NOT** be loaded in a production environment
+
+Copyright (c) 2018 The Fuel Rat Mischief,
+All rights reserved.
+
+Licensed under the BSD 3-Clause License.
+
+See LICENSE.md
+"""
+import logging
+
+from src.packages.context.context import Context
+from src.packages.permissions.permissions import require_permission, TECHRAT, require_channel
+from src.packages.rat_command.rat_command import command
+
+log = logging.getLogger(f"mecha.{__name__}")
+
+
+@command("debug-whois")
+@require_channel
+@require_permission(TECHRAT)
+async def cmd_debug_whois(context):
+    """A debug command for running a WHOIS command.
+
+    Returns
+        str: string repreentation
+    """
+    data = await context.bot.whois(context.words[1])
+    log.debug(data)
+    await context.reply(f"{data}")
+
+
+@command("debug-userinfo")
+@require_permission(TECHRAT)
+@require_channel
+async def cmd_debug_userinfo(context: Context):
+    await context.reply(f"triggering user is {context.user.nickname}, {context.user.hostname}")
+    await context.reply(f"user identifed?: {context.user.identified}")
+
+
+@command("superPing!")
+@require_channel
+@require_permission(TECHRAT)
+async def cmd_superping(context: Context):
+    await context.reply("pong!")
+
