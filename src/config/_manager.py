a7023a8229cffa91540bada62eeccfb30ca4409a
Orion (Theunkn0wn1) <thhunkn0wnd@gmail.com>
2019-04-23 10:38:53 -0700

    SPARK-130 corrected bad documentation in _manager.py

--- src/config/_manager.py
+++ src/config/_manager.py
@@ -1,7 +1,8 @@
 """
-$file.fileName - $shortDescripion  # FIXME template
+_manager.py  Configuration related plugin management
 
-$longDescription
+contains the configuration plugin manager
+
 Copyright (c) 2019 The Fuel Rats Mischief,
 All rights reserved.
 
@@ -10,15 +11,6 @@ Licensed under the BSD 3-Clause License.
 See LICENSE.md
 """
 
-#
-#  Copyright (c) $DateInfo.year The Fuel Rats Mischief,
-#  All rights reserved.
-#
-#  Licensed under the BSD 3-Clause License.
-#
-#  See LICENSE.md
-#
-
 import logging
 
 import pluggy
@@ -26,6 +18,7 @@ import pluggy
 from ._constants import _PLUGIN_NAME
 
 LOG = logging.getLogger(f"mecha.{__name__}")
+
 PLUGIN_MANAGER = pluggy.PluginManager(_PLUGIN_NAME)
 """
 Configuration plugin manager
