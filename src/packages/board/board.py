723fa5e3e85d7af4785785811a41d9a5828eb4a1
Orion (Theunkn0wn1) <thhunkn0wnd@gmail.com>
2019-05-06 17:25:11 -0700

    SPARK-52 Begin completely reimplementing the board

---
+++ src/packages/board/board.py
@@ -0,0 +1,198 @@
+"""
+$file.fileName - 
+
+
+Copyright (c) 2019 The Fuel Rats Mischief,
+All rights reserved.
+
+Licensed under the BSD 3-Clause License.
+
+See LICENSE.md
+"""
+from __future__ import annotations
+
+import itertools
+import logging
+import typing
+from collections import abc
+from contextlib import asynccontextmanager
+from uuid import UUID
+
+from src.packages.rescue import Rescue
+
+CYCLE_AT = 15
+"""
+Determines at what board index does mecha (attempt) to start over indexing
+
+Notes:
+    mecha will still count beyond this value unrestricted, but will attempt
+    to keep assigned case numbers below this value whenever possible.
+"""
+_KEY_TYPE = typing.Union[str, int, UUID]
+
+LOG = logging.getLogger(f"mecha.{__name__}")
+
+
+class RatBoard(abc.Mapping):
+    """
+    The Rat Board
+    """
+    __slots__ = ["_storage_by_uuid",
+                 "_storage_by_client",
+                 "_handler",
+                 "_storage_by_index",
+                 "_index_counter"]
+
+    def __init__(self, api_handler=None):
+        self._handler = api_handler
+        """
+        fuelrats.com API handler
+        """
+        self._storage_by_uuid: typing.Dict[UUID, Rescue] = {}
+        """
+        internal rescue storage keyed by uuid
+        """
+        self._storage_by_client: typing.Dict[str, Rescue] = {}
+        """
+        internal rescue storage keyed by client
+        """
+        self._storage_by_index: typing.Dict[int, Rescue] = {}
+        """
+        internal rescue storage keyed by board index
+        """
+        self._index_counter = itertools.count()
+        """
+        Internal counter for tracking used indexes
+        """
+
+    def __getitem__(self, key: _KEY_TYPE) -> Rescue:
+        if isinstance(key, str):
+            return self._storage_by_client[key]
+
+        if isinstance(key, UUID):
+            return self._storage_by_uuid[key]
+
+        if isinstance(key, int):
+            return self._storage_by_index[key]
+
+        # not one of our key types,
+        return super(RatBoard, self).__getitem__(key)
+
+    def __len__(self) -> int:
+        return len(self._storage_by_uuid)
+
+    def __iter__(self) -> typing.Iterator[UUID]:
+        return iter(self._storage_by_uuid)
+
+    @property
+    def _free_case_number(self) -> int:
+        """
+        returns the next unused index in the series.
+
+        Returns:
+            int: next free board index
+        """
+        # this line is so magical it gets its own method.
+        # basically, this returns the next index from self._index_counter'
+        # that is not already in use (contains & filterfalse)
+        return next(itertools.filterfalse(self.__contains__, self._index_counter))
+
+    @property
+    def free_case_number(self) -> int:
+        """
+        Returns a unused case number
+
+        Returns:
+            int: unused case number
+
+        Notes:
+            This method will attempt to return values smaller than the defined
+            :obj:`CYCLE_AT` whenever possible, though will return values
+            in excess of :obj:`CYCLE_AT` as necessary.
+        """
+        # if we are larger or equal to the CYCLE_AT point, reset the counter
+        if self._free_case_number >= CYCLE_AT:
+            self._index_counter = itertools.count()
+
+        # return the next index from the magic
+        return self._free_case_number
+
+    def __contains__(self, key: _KEY_TYPE) -> bool:
+        return (key in self._storage_by_client or
+                key in self._storage_by_uuid or
+                key in self._storage_by_index
+                )
+
+    def __delitem__(self, key: _KEY_TYPE):
+        if key not in self:
+            raise KeyError(key)
+
+        # get the target
+        target = self[key]
+
+        # and purge it key by key
+        del self._storage_by_uuid[target.api_id]
+        del self._storage_by_index[target.board_index]
+        if target.client:
+            del self._storage_by_client
+
+    @asynccontextmanager
+    async def create_rescue(self, *args, **kwargs) -> Rescue:
+        """
+        Context manager that creates a rescue on the board for use
+
+        automatically assigns
+
+        Args:
+            pass through to :class:Rescue 's constructor
+
+            *args ():
+            **kwargs ():
+
+        Yields:
+            created rescue object
+        """
+
+        rescue = Rescue(*args, board_index=self.free_case_number, **kwargs)
+        yield rescue
+        # then append it to ourselves
+        self._append(rescue)
+
+    def _append(self, rescue: Rescue) -> None:
+        """
+        Actually append the object to ourselves
+
+        Args:
+            rescue (Rescue): object to append
+    """
+        self._storage_by_uuid[rescue.api_id] = rescue
+        self._storage_by_index[rescue.board_index] = rescue
+
+        if rescue.client:
+            self._storage_by_client[rescue.client] = rescue
+
+    @asynccontextmanager
+    async def modify_rescue(self, key: _KEY_TYPE) -> Rescue:
+        """
+        Context manager to modify a Rescue
+
+        Args:
+            key ():
+
+        Returns:
+
+        """
+
+        target = self[key]
+
+        # nickname and index are mutable, and used for indexing
+        # so we need to track if either of these actually changed
+        old_nickname = target.client
+        old_index = target.board_index
+        yield target
+
+        # client changed, reindex that key
+        if old_nickname != target.client:
+            ...
+
+        # FIXME complete
