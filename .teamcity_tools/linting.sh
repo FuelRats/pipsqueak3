305bf6f8ab233a4b9fdd3c183e64333449e999b0
Joshua Salzedo <thHunkn0WNd@gmail.com>
2019-07-04 16:05:51 -0700

    [TEAMCITY] remove trace of `pipenv run`  in linting script

--- .teamcity_tools/linting.sh
+++ .teamcity_tools/linting.sh
@@ -2,7 +2,7 @@
 echo "ls -a..."
 ls -a
 echo "running pylint"....
-pylint src || pipenv run python pylint-exit $?
+pylint src || python pylint-exit $?
 echo "running pycodestyle..."
 pycodestyle src
 echo "done!"
