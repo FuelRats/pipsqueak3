61d7d68dce14820e7eb57715820461c855b5772a
Joshua Salzedo <thHunkn0WNd@gmail.com>
2019-07-04 14:24:28 -0700

    [TEAMCITY] bash may not be as intuitive as it looks

--- .teamcity/get_build_id.sh
+++ .teamcity/get_build_id.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# get the current commit from git
-GIT_HASH=git rev-parse --short HEAD
-echo "GIT_HASH=${GIT_HASH}"
-GIT_HASH_SHORT=${GIT_HASH:0:7}
-echo "GIT_HASH_SHORT=${GIT_HASH_SHORT}"
+# get current commit from git
+GIT_HASH="$(git rev-parse --short HEAD)"
+echo "full commit hash: ${GIT_HASH}"
+GIT_HASH_SHORT="${GIT_HASH:0:7}"
+echo "output hash : ${GIT_HASH_SHORT}"
 echo "##teamcity[buildNumber '${GIT_HASH_SHORT}']"
