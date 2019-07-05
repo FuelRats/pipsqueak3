8ac0734b4e35f1d883f109a5e5d7229a644e43b0
Eearslya Sleiarion <eearslya@gmail.com>
2019-07-05 10:25:26 -0700

    Fix the TeamCity error message format

--- .teamcity_tools/linting.sh
+++ .teamcity_tools/linting.sh
@@ -3,7 +3,7 @@ echo "ls -a..."
 ls -a
 echo "running pylint"....
 pylint src
-python pylint-exit $? || echo "##teamcity[buildProblem='pylint failed!']"
+python pylint-exit $? || echo "##teamcity[buildProblem description='pylint failed!']"
 echo "running pycodestyle..."
 pycodestyle src
 echo "done!"
