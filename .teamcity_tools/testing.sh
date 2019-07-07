d9e061391aa4d5b2ab0eb33138cb76e72b5d30d1
Joshua Salzedo <thHunkn0WNd@gmail.com>
2019-07-06 17:18:58 -0700

    [TEAMCITY] It helps to point pytest at the right base directory.

--- .teamcity_tools/testing.sh
+++ .teamcity_tools/testing.sh
@@ -6,5 +6,5 @@ source /mechasqueak/.teamcity_tools/shared_lib.sh
 check_cwd
 
 echo "##teamcity[blockOpened name='Execute test suite']"
-pytest src
+pytest tests
 echo "##teamcity[blockClosed name='Execute test suite']"
