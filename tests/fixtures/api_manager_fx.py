9ceaf688b921cd991e59ba3a13fc5f7d0d04d16a
Eearslya Sleiarion <eearslya@gmail.com>
2019-07-12 11:43:28 -0700

    [SPARK-2] Start using real mock tests
    [ci skip]

--- tests/fixtures/api_manager_fx.py
+++ tests/fixtures/api_manager_fx.py
@@ -22,10 +22,46 @@ def mock_api_server_fx():
     """
     # pylint: disable=line-too-long
 
-    #with HTTPServer('127.0.0.1', '4001') as httpserver:
+    with HTTPServer('127.0.0.1', '4001') as httpserver:
+        ### Rat Data ###
+        # - rat1
+        httpserver.expect_request("/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d").respond_with_data(
+            """
+{"data": {"id": "8dfb8593-4694-44f2-af2f-686b1e6cf48d", "type": "rats", "attributes": {"name": "rat1", "data": null, "platform": "pc", "createdAt": "2019-07-11T09:14:48.441770+02:00", "updatedAt": "2019-07-11T09:14:48.441884+02:00"}, "links": {"self": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d"}, "related": {}, "relationships": {"user": {"data": null, "links": {"self": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d/relationships/user", "related": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d/user"}, "meta": {"direction": "MANYTOONE", "results": {}}}}, "meta": {"forbidden_fields": []}}, "included": [], "links": {"self": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d"}, "meta": {}}"""
+        )
+
+        ### Rat Searches ###
+        # - name: rat1
+        httpserver.expect_request("/rats", query_string=b"filter%5Bname:ilike%5D=rat1").respond_with_data(
+            """{"data": [{"id": "8dfb8593-4694-44f2-af2f-686b1e6cf48d", "type": "rats", "attributes": {"name": "rat1", "data": null, "platform": "pc", "createdAt": "2019-07-11T09:14:48.441770+02:00", "updatedAt": "2019-07-11T09:14:48.441884+02:00"}, "links": {"self": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d"}, "related": {}, "relationships": {"user": {"data": null, "links": {"self": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d/relationships/user", "related": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d/user"}, "meta": {"direction": "MANYTOONE", "results": {}}}}, "meta": {"forbidden_fields": []}}], "included": [], "links": {"first": "http://127.0.0.1/api/rats?sort=id&filter%5Bname%3Ailike%5D=rat1&page%5Boffset%5D=0", "last": "http://127.0.0.1/api/rats?sort=id&filter%5Bname%3Ailike%5D=rat1&page%5Boffset%5D=0", "self": "http://127.0.0.1/api/rats?filter[name:ilike]=rat1"}, "meta": {"results": {"available": 1, "limit": 10, "offset": 0, "returned": 1}}}"""
+        )
+
+        # - name: rat1
+        # - platform: pc
+        httpserver.expect_request("/rats", query_string=b"filter%5Bname:ilike%5D=rat1&filter%5Bplatform:eq%5D=pc").respond_with_data(
+            """{"data": [{"id": "8dfb8593-4694-44f2-af2f-686b1e6cf48d", "type": "rats", "attributes": {"name": "rat1", "data": null, "platform": "pc", "createdAt": "2019-07-11T09:14:48.441770+02:00", "updatedAt": "2019-07-11T09:14:48.441884+02:00"}, "links": {"self": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d"}, "related": {}, "relationships": {"user": {"data": null, "links": {"self": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d/relationships/user", "related": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d/user"}, "meta": {"direction": "MANYTOONE", "results": {}}}}, "meta": {"forbidden_fields": []}}], "included": [], "links": {"first": "http://127.0.0.1/api/rats?sort=id&filter%5Bname%3Ailike%5D=rat1&filter%5Bplatform%3Aeq%5D=pc&page%5Boffset%5D=0", "last": "http://127.0.0.1/api/rats?sort=id&filter%5Bname%3Ailike%5D=rat1&filter%5Bplatform%3Aeq%5D=pc&page%5Boffset%5D=0", "self": "http://127.0.0.1/api/rats?filter[name:ilike]=rat1&filter[platform:eq]=pc"}, "meta": {"results": {"available": 1, "limit": 10, "offset": 0, "returned": 1}}}"""
+        )
+
+        ### Rescue Data ###
+        # Open rescue with rat1 assigned
+        httpserver.expect_request("/rescues/26560069-ff2f-485d-bd11-8cb878b570c0", query_string=b"include=rats").respond_with_data(
+            """{"data": {"id": "26560069-ff2f-485d-bd11-8cb878b570c0", "type": "rescues", "attributes": {"client": "pcClient", "codeRed": false, "data": {"langID": "en", "status": {}, "IRCNick": "pcClient", "boardIndex": 24, "markedForDeletion": {"marked": false, "reason": null, "reporter": null}}, "notes": null, "platform": "pc", "quotes": [], "status": "open", "system": "FIRESTONE", "title": null, "outcome": null, "unidentifiedRats": [], "createdAt": "2019-07-11T09:15:07.431666+02:00", "updatedAt": "2019-07-11T09:15:07.431724+02:00"}, "links": {"self": "http://127.0.0.1/api/rescues/26560069-ff2f-485d-bd11-8cb878b570c0"}, "related": {}, "relationships": {"firstLimpet": {"data": null, "links": {"self": "http://127.0.0.1/api/rescues/26560069-ff2f-485d-bd11-8cb878b570c0/relationships/firstLimpet", "related": "http://127.0.0.1/api/rescues/26560069-ff2f-485d-bd11-8cb878b570c0/firstLimpet"}, "meta": {"direction": "MANYTOONE", "results": {}}}, "rats": {"data": [{"type": "rats", "id": "8dfb8593-4694-44f2-af2f-686b1e6cf48d"}], "links": {"self": "http://127.0.0.1/api/rescues/26560069-ff2f-485d-bd11-8cb878b570c0/relationships/rats", "related": "http://127.0.0.1/api/rescues/26560069-ff2f-485d-bd11-8cb878b570c0/rats"}, "meta": {"direction": "MANYTOMANY", "results": {"limit": 10, "available": 1, "returned": 1}}}}, "meta": {"forbidden_fields": []}}, "included": [{"id": "8dfb8593-4694-44f2-af2f-686b1e6cf48d", "type": "rats", "attributes": {"name": "rat1", "data": null, "platform": "pc", "createdAt": "2019-07-11T09:14:48.441770+02:00", "updatedAt": "2019-07-11T09:14:48.441884+02:00"}, "links": {"self": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d"}, "related": {}, "relationships": {"user": {"data": null, "links": {"self": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d/relationships/user", "related": "http://127.0.0.1/api/rats/8dfb8593-4694-44f2-af2f-686b1e6cf48d/user"}, "meta": {"direction": "MANYTOONE", "results": {}}}}, "meta": {"forbidden_fields": []}}], "links": {"self": "http://127.0.0.1/api/rescues/26560069-ff2f-485d-bd11-8cb878b570c0?include=rats"}, "meta": {}}"""
+        )
+
+        # 404 response for id "random-uuid"
+        httpserver.expect_request("/rescues/random-uuid", query_string=b"include=rats").respond_with_data(
+            """{"errors": [{"code": "404", "detail": "Object random-uuid not found in collection rescues", "title": "Not Found"}]}""",
+            status=404
+        )
+
+        yield httpserver
 
 
 @pytest.fixture(scope='session')
-def api_manager_fx(mock_api_server_fx) -> APIManager:
-    config = {'api': {'url': 'https://apiv3.fuelrats.dev/api/', 'online_mode': True}}
+def api_manager_fx(mock_api_server_fx) -> APIManager:  # pylint: disable=redefined-outer-name
+    """
+    Test fixture for APIManager. Includes a mock API server with pre-made calls.
+    """
+    base_url = mock_api_server_fx.url_for('/')
+    config = {'api': {'url': base_url, 'online_mode': True}}
     return APIManager(config)
--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -9,15 +9,13 @@ Licensed under the BSD 3-Clause License.
 See LICENSE
 """
 
-import asyncio
 import logging
+from uuid import UUID
 import pytest
 
-import aiohttp
+from src.packages.utils import Platforms, Status
 
-from src.packages.utils import Platforms
-
-pytestmark = [pytest.mark.unit, pytest.mark.api]
+pytestmark = [pytest.mark.unit, pytest.mark.api]  # pylint: disable=invalid-name
 
 LOG = logging.getLogger("apitest")
 
@@ -28,41 +26,83 @@ async def test_find_rat_by_uuid(api_manager_fx):
     Test that we can find a rat via their UUID through the API.
     """
     rat = await api_manager_fx.get_rat(uuid='8dfb8593-4694-44f2-af2f-686b1e6cf48d')
+    assert rat is not None
+    assert rat.uuid == UUID('8dfb8593-4694-44f2-af2f-686b1e6cf48d')
+    assert rat.name == 'rat1'
+    assert rat.platform == Platforms.PC
 
 
 @pytest.mark.asyncio
 async def test_find_rat_by_name(api_manager_fx):
+    """
+    Test that we can find a rat via their name.
+    """
     rat = await api_manager_fx.find_rat(name='rat1')
+    assert rat is not None
+    assert rat.uuid == UUID('8dfb8593-4694-44f2-af2f-686b1e6cf48d')
+    assert rat.name == 'rat1'
+    assert rat.platform == Platforms.PC
 
 
 @pytest.mark.asyncio
 async def test_find_rat_by_name_and_platform(api_manager_fx):
+    """
+    Test that we can find a rat by both name and platform.
+    """
     rat = await api_manager_fx.find_rat(name='rat1', platform=Platforms.PC)
+    assert rat is not None
+    assert rat.uuid == UUID('8dfb8593-4694-44f2-af2f-686b1e6cf48d')
+    assert rat.name == 'rat1'
+    assert rat.platform == Platforms.PC
 
 
 @pytest.mark.asyncio
 async def test_find_rescue_by_uuid(api_manager_fx):
-    rescue = await api_manager_fx.get_rescue('4f3a8068-028a-4e0c-9357-947105ec6dc5')
-    assert not rescue is None
+    """
+    Test that we can find a rescue by its UUID.
+    """
+    rescue = await api_manager_fx.get_rescue('26560069-ff2f-485d-bd11-8cb878b570c0')
+    assert rescue is not None
+    assert rescue.status == Status.OPEN
+    assert rescue.rats
+    assert rescue.rats[0].name == 'rat1'
+
 
 @pytest.mark.asyncio
 async def test_find_rescue_by_uuid_invalid(api_manager_fx):
+    """
+    Test that trying to find a rescue that doesn't exist returns None.
+    """
     rescue = await api_manager_fx.get_rescue('random-uuid')
     assert rescue is None
 
-@pytest.mark.asyncio
-async def test_update_rescue(api_manager_fx):
-    new_data = {'data': {'IRCNick':'Eearslya2'}}
-    #await api_manager_fx.update_rescue('6e9d808d-7d60-4220-ad24-4ababf2a7b29', new_data)
 
-
+@pytest.mark.skip(reason='WIP')
 @pytest.mark.asyncio
 async def test_create_rescue(api_manager_fx, rescue_sop_fx):
-    pass
-    #rescue = await api_manager_fx.create_rescue(rescue_sop_fx)
-    #breakpoint()
+    """
+    Test that we can create a rescue in the API.
+    """
+    rescue = await api_manager_fx.create_rescue(rescue_sop_fx)
+    assert rescue.client == rescue_sop_fx.client
+    assert rescue.platform == rescue_sop_fx.platform
+    assert rescue.system == rescue_sop_fx.system
+
+
+@pytest.mark.skip(reason='WIP')
+@pytest.mark.asyncio
+async def test_update_rescue(api_manager_fx, rescue_sop_fx):
+    """
+    Test that we can perform a full update of a rescue in the API.
+    """
+    await api_manager_fx.update_rescue(rescue_sop_fx)
 
 
 @pytest.mark.asyncio
 async def test_rescue_serialize(api_manager_fx, rescue_sop_fx):
-    rescue = api_manager_fx._serialize_rescue(rescue_sop_fx)
+    """
+    Assert that we can serialize a Rescue object into a properly-formatted Dict.
+    """
+    rescue = api_manager_fx._serialize_rescue(rescue_sop_fx)  # pylint: disable=protected-access
+    assert rescue
+    assert rescue['attributes']
