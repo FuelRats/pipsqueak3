004b6b5fac737a06d6fb44383778a1e06299e24b
Joshua Salzedo <thHunkn0WNd@gmail.com>
2019-12-07 18:05:45 -0800

    Squash add tests from old branch

---
+++ tests/unit/api/test_mock_v3.py
@@ -0,0 +1,29 @@
+from uuid import UUID
+
+from src.packages.fuelrats_api.v3.mockup import MockupAPI
+
+import pytest
+
+from src.packages.rescue import Rescue
+
+pytestmark = [pytest.mark.fuelrats_api, pytest.mark.asyncio, pytest.mark.integration]
+
+
+@pytest.fixture
+def mock_fuelrats_api_fx():
+    # TODO pull from configuration system
+    return MockupAPI(url=r'http://api.thehellisthis.com:6543/api')
+
+
+async def test_get_rescue(mock_fuelrats_api_fx, ):
+    target = UUID("c1ff8456-5c51-47b1-8d0e-be6104840820")
+    result = await mock_fuelrats_api_fx.get_rescue(target)
+
+    assert isinstance(result, Rescue)
+
+
+async def test_update_rescue(mock_fuelrats_api_fx, rescue_sop_fx):
+    target = UUID("c1ff8456-5c51-47b1-8d0e-be6104840820")
+    rescue_sop_fx._api_id = target
+
+    await mock_fuelrats_api_fx.update_rescue(rescue_sop_fx)
