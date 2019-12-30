132308c0ac957ded54852c5cd38f39acce97656b
Joshua Salzedo <thHunkn0WNd@gmail.com>
2019-12-29 18:56:34 -0800

    Cleaned up old implementation, this is simpler

--- src/packages/fuelrats_api/v3/_dataclasses/v1/meta.py
+++ src/packages/fuelrats_api/v3/_dataclasses/v1/meta.py
@@ -6,6 +6,10 @@ import attr
 @attr.dataclass
 class Meta:
     rateLimitTotal: int
+    """The total of number requests per hour for the current client"""
     rateLimitRemaining: int
+    """The remaining number of requests allowed for this client for the hour"""
     rateLimitReset: datetime
+    """Timestamp for when rate limit usage will reset (top of the next hour)"""
     apiVersion: str
+    """The current Fuel Rats API version\n"""
