1cf9d310978402f4c3879156c17efea5a6f398a0
joshua salzedo <jsalzedo@appliedinvention.com>
2020-02-29 15:44:14 -0800

    attempted to implement !landmark-near

--- src/commands/starsystems.py
+++ src/commands/starsystems.py
@@ -10,3 +10,15 @@ async def cmd_search(ctx: Context):
         return await ctx.reply("Usage: search <name of system>")
     results = await ctx.bot.galaxy.search_systems_by_name(ctx.words[1])
     return await ctx.reply(f"{results!r}")
+
+
+@permissions.require_permission([permissions.RAT])
+@command("landmark-near")
+async def cmd_landmark_near(ctx: Context):
+    if len(ctx.words) != 2:
+        return await ctx.reply("Usage: landmark near <system>")
+
+    found = await ctx.bot.galaxy.find_system_by_name(ctx.words[1].upper())
+    if not found:
+        return await ctx.reply(f"{ctx.words[1]} was not found in The Fuel Rats System Database.")
+    return await ctx.bot.galaxy.find_nearest_landmark(found)
