1c64e4d8ea8474ecf33f6235d14dd2174708a371
joshua salzedo <jsalzedo@appliedinvention.com>
2020-03-01 11:55:46 -0800

    additional starsystem diagnostics

--- src/commands/starsystems.py
+++ src/commands/starsystems.py
@@ -1,7 +1,7 @@
 from ..packages.context import Context
 from ..packages.commands import command
 from ..packages import permissions
-
+from loguru import logger
 
 @permissions.require_permission(permissions.RAT)
 @command("search")
@@ -18,7 +18,12 @@ async def cmd_landmark_near(ctx: Context):
     if len(ctx.words) != 2:
         return await ctx.reply("Usage: landmark near <system>")
 
-    found = await ctx.bot.galaxy.find_system_by_name(ctx.words[1].upper())
+    system_name = ctx.words[1]
+    logger.trace("searching for system {}", system_name)
+    found = await ctx.bot.galaxy.find_system_by_name(system_name)
     if not found:
-        return await ctx.reply(f"{ctx.words[1]} was not found in The Fuel Rats System Database.")
-    return await ctx.bot.galaxy.find_nearest_landmark(found)
+        return await ctx.reply(f"{system_name} was not found in The Fuel Rats System Database.")
+
+    logger.debug("found system {}, acquiring nearest landmark...", found)
+    nearest_landmark = await ctx.bot.galaxy.find_nearest_landmark(found)
+    return await ctx.reply(f"{nearest_landmark}")
