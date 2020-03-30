383f4c4d00fe61bfee0c0769a05c70fb62dd8118
joshua salzedo <thHunkn0WNd@gmail.com>
2020-03-30 13:10:04 -0700

    Added test for clear

--- tests/unit/commands/test_board.py
+++ tests/unit/commands/test_board.py
@@ -21,15 +21,36 @@ async def test_active(rat_board_fx, bot_fx, initial: bool, expected: bool):
 @pytest.mark.parametrize("names", [("foo", "bar", "baz"), ("snafu",)])
 async def test_assign_unidentified(rescue_sop_fx, bot_fx, names):
     await bot_fx.board.append(rescue_sop_fx)
-    ctx = await Context.from_message(bot=bot_fx, channel="#unkn0wndev", sender="some_ov",
-                                     message=f"!assign {rescue_sop_fx.board_index} {' '.join(names)}")
+    ctx = await Context.from_message(
+        bot=bot_fx,
+        channel="#unkn0wndev",
+        sender="some_ov",
+        message=f"!assign {rescue_sop_fx.board_index} {' '.join(names)}",
+    )
     await case_management.cmd_case_management_assign(ctx)
     for name in names:
-        assert name in rescue_sop_fx.unidentified_rats, "failed to assign unidentified rats"
+        assert (
+            name in rescue_sop_fx.unidentified_rats
+        ), "failed to assign unidentified rats"
 
     # verify unassign behavior while we are here (less duplication than a distinct test)
-    ctx = await Context.from_message(bot=bot_fx, channel="#unkn0wndev", sender="some_ov",
-                                     message=f"!unassign {rescue_sop_fx.board_index} {' '.join(names)}")
+    ctx = await Context.from_message(
+        bot=bot_fx,
+        channel="#unkn0wndev",
+        sender="some_ov",
+        message=f"!unassign {rescue_sop_fx.board_index} {' '.join(names)}",
+    )
     await case_management.cmd_case_management_unassign(ctx)
     for name in names:
-        assert name not in rescue_sop_fx.unidentified_rats, "failed to unassign unidentified rats"
+        assert (
+            name not in rescue_sop_fx.unidentified_rats
+        ), "failed to unassign unidentified rats"
+
+
+async def test_clear_no_rat(rescue_sop_fx, bot_fx):
+    await bot_fx.board.append(rescue_sop_fx)
+    ctx = await Context.from_message(
+        bot_fx, "#unkn0wndev", "some_ov", f"!clear {rescue_sop_fx.board_index}"
+    )
+    await case_management.cmd_case_management_clear(ctx)
+    assert rescue_sop_fx.board_index not in bot_fx.board, "failed to clear rescue"
