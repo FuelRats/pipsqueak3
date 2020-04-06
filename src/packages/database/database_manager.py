2a00b5cc4a946a38c57a784f1f595a81e8861fa8
joshua salzedo <thHunkn0WNd@gmail.com>
2020-04-06 10:50:11 -0700

    don't log queries in production builds

--- src/packages/database/database_manager.py
+++ src/packages/database/database_manager.py
@@ -174,7 +174,8 @@ class DatabaseManager:
             connection.set_client_encoding('utf-8')
             # Create cursor, and execute the query.
             with connection.cursor() as cursor:
-                logger.debug("executing query {}", query)
+                if __debug__:
+                    logger.debug("executing query {}", query)  # noinspection PyUnreachableCode
                 cursor.execute(query, values)
                 # Check if cursor.description is NONE - meaning no results returned.
                 if cursor.description:
