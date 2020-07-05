b11171dae7c2ed62a1db5cf1c8674d3d14beb1b1
joshua salzedo <thHunkn0WNd@gmail.com>
2020-07-05 16:34:04 -0700

    [SPARK-3] API Error parsing

---
+++ src/packages/fuelrats_api/v3/models/v1/apierror.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from typing import Dict, Optional
+
+import attr
+from ..jsonapi.link import Links
+from uuid import UUID, uuid4
+
+
+@attr.dataclass
+class Pointer:
+    pointer: Optional[str] = attr.ib(
+        validator=attr.validators.optional(attr.validators.instance_of(str)), default=None)
+    """
+    If the error is a client error caused by an invalid entity in the request object a JSON pointer
+    to it will be provided here.
+    """
+    parameter: str = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(str)),
+                             default=None)
+    """
+    If the error is a client error caused by a query parameter the name of the query parameter will 
+    be provided here.
+    """
+
+    @classmethod
+    def from_dict(cls, data: Dict) -> Pointer:
+        return cls(**data)
+
+
+@attr.dataclass
+class ApiError:
+    id_: UUID = attr.ib(converter=UUID)
+    links: Links = attr.ib(validator=attr.validators.instance_of(dict))
+    status: str = attr.ib(validator=attr.validators.instance_of(str))
+    code: int = attr.ib(validator=attr.validators.instance_of(int))
+    title: str = attr.ib(validator=attr.validators.instance_of(str))
+    detail: str = attr.ib(validator=attr.validators.instance_of(str))
+    source: Pointer = attr.ib(validator=attr.validators.instance_of(Pointer))
+
+    @classmethod
+    def from_dict(cls, data: Dict) -> ApiError:
+        data['id_'] = data['id']
+        del data['id']
+        data['source'] = Pointer.from_dict(data['source'])
+        return cls(**data)
+
