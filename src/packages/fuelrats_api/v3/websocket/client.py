b7f4e3df750428b9cabdd02c9ad21d90a2f4e9b1
joshua salzedo <thHunkn0WNd@gmail.com>
2020-07-08 15:42:09 -0700

    [SPARK-2] Fix connection race condition | handle Events

--- src/packages/fuelrats_api/v3/websocket/client.py
+++ src/packages/fuelrats_api/v3/websocket/client.py
@@ -1,15 +1,16 @@
 import asyncio
-from typing import Dict, Coroutine
+import json
+from typing import Dict
 
-from websockets.client import WebSocketClientProtocol
-from .protocol import Response, Request
 from loguru import logger
+from websockets.client import WebSocketClientProtocol
 
+from .protocol import Response, Request, Event
 from ..models.v1.apierror import APIException, ApiError
 
 
 class Connection:
-    __slots__ = ["_socket", "_futures", "shutdown", "_work"]
+    __slots__ = ["_socket", "_futures", "shutdown", "_work", "_rx_worker", "_tx_worker"]
 
     def __init__(self, socket):
         self._socket: WebSocketClientProtocol = socket
@@ -18,30 +19,41 @@ class Connection:
         self._work: asyncio.Queue[Request] = asyncio.Queue()
 
         # spawn worker tasks
-        asyncio.create_task(self.rx_worker())
-        asyncio.create_task(self.tx_worker())
+        self._rx_worker = asyncio.create_task(self.rx_worker())
+        self._tx_worker = asyncio.create_task(self.tx_worker())
+
+    async def _handle_response(self, response: Response):
+        logger.debug("parsed response:= {!r}", response)
+        # check if we had a future for this, if so complete it.
+        if response.state in self._futures:
+            # if its an error return, then set the exception so the consumer raises.
+            if response.status != 200:
+                self._futures[response.state].set_exception(
+                    APIException(ApiError.from_dict(response.body["errors"][0]))
+                )
+                return
+            self._futures[response.state].set_result(response)
+        else:
+            if response.status != 200:
+                raise APIException(ApiError.from_dict(response.body["errors"][0]))
+            logger.warning("got unsolicited response {!r}", response)
+
+    async def _handle_event(self, event: Event):
+        logger.debug("recv'ed API event {!r}", event)
 
     async def rx_worker(self):
         """ worker that receives messages from the websocket """
         while not self.shutdown.is_set():
             # async block read from socket
             raw = await self._socket.recv()
-            response = Response.deserialize(raw)
-
-            logger.debug("parsed response:= {!r}", response)
-            # check if we had a future for this, if so complete it.
-            if response.state in self._futures:
-                # if its an error return, then set the exception so the consumer raises.
-                if response.status != 200:
-                    self._futures[response.state].set_exception(
-                        APIException(ApiError.from_dict(response.body["errors"][0]))
-                    )
-                    continue
-                self._futures[response.state].set_result(response)
-            else:
-                if response.status != 200:
-                    raise APIException(ApiError.from_dict(response.body["errors"][0]))
-                logger.warning("got unsolicited response {!r}", response)
+            raw_data = json.loads(raw)
+            if len(raw_data) == 3:
+                response = Response(*raw_data)
+                await self._handle_response(response)
+                continue
+            if len(raw_data) == 4:
+                event = Event(*raw_data)
+                await self._handle_event(event)
 
     async def tx_worker(self):
         """ Worker that sends messages to the websocket """
@@ -50,7 +62,7 @@ class Connection:
             work = await self._work.get()
             await self._socket.send(work.serialize())
 
-    def execute(self, work: Request) -> asyncio.Future:
+    async def execute(self, work: Request) -> Response:
 
         # create a future, representing the Response that will satisfy this work item
         loop = asyncio.get_event_loop()
@@ -59,5 +71,15 @@ class Connection:
         # submit the item to the queue, for the tx_worker to pick up
         self._work.put_nowait(work)
 
-        # return the future to the caller,who can then await the future
-        return future
+        # await the future to complete in another task.
+        result = await future
+
+        # check if either of our workers exploded on us
+        # we do this here since exceptions cannot otherwise be meaningfully handled,
+        # as the caller of this method is probably not doing so from a unmonitored task
+        if self._rx_worker.done() and self._rx_worker.exception():
+            raise self._rx_worker.exception()
+        if self._tx_worker.done() and self._tx_worker.exception():
+            raise self._tx_worker.exception()
+
+        return result
