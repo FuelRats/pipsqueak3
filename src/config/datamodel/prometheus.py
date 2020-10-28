c00544b512cce1b4f9701c8d2f91ffa33b289e47
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-10-28 12:30:06 -0700

    [SPARK-235] use the correct localhost ip address...

--- src/config/datamodel/prometheus.py
+++ src/config/datamodel/prometheus.py
@@ -10,7 +10,7 @@ IPAddress = Union[IPv6Address, IPv4Address]
 class TelemetryConfigRoot:
     bind_host: IPAddress = attr.ib(
         validator=attr.validators.instance_of((IPv4Address, IPv6Address)),
-        default=ip_address("172.0.0.1"),
+        default=ip_address("127.0.0.1"),
     )
     bind_port: int = attr.ib(validator=attr.validators.instance_of(int), default=6820)
     enabled: bool = attr.ib(validator=attr.validators.instance_of(bool), default=False)
