f25339bb4436f299759e7ae755766fb8a391da02
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-11-20 12:14:46 -0800

    [SPARK-239] Added regression test for SPARK-239

--- tests/regressions/test_case_management_regressions.py
+++ tests/regressions/test_case_management_regressions.py
@@ -29,3 +29,24 @@ async def test_spark_223_command(bot_fx):
     await trigger(ctx=context)
 
     assert len(bot_fx.board) == pre_len, "number of rescues changed unexpectedly!"
+
+
+async def test_spark_239(bot_fx, rescue_sop_fx, monkeypatch):
+    await bot_fx.board.append(rescue_sop_fx)
+    assign_payload = f"!assign {rescue_sop_fx.board_index} Unknown"
+    context = await Context.from_message(bot=bot_fx, channel="#fuelrats", sender="some_ov",
+                                         message=assign_payload)
+
+    await trigger(context)
+    assert "unknown" in rescue_sop_fx.unidentified_rats
+
+    assign_payload = f"!clear {rescue_sop_fx.board_index} unknown"
+    context = await Context.from_message(bot=bot_fx, channel="#fuelrats", sender="some_ov",
+                                         message=assign_payload)
+    # since this test is against an unidentified rat, this is only gunna work in drill mode.
+    # drill mode can be faked by setting DRILL_MODE on context.
+    monkeypatch.setattr(context, "DRILL_MODE", True)
+
+    await trigger(context)
+
+    assert rescue_sop_fx.client not in bot_fx.board
