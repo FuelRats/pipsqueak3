92fe87935f810ae0b829c562486ebd179c4501f7
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-31 13:09:10 -0800

    Oops, testing code snuck in

--- src/templates/__init__.py
+++ src/templates/__init__.py
@@ -20,20 +20,6 @@ from src.packages.rescue import Rescue
 from src.packages.utils.ratlib import Platforms, Status, Colors, color
 
 
-async def inject_cases(board: RatBoard):
-    snafu = await board.create_rescue(client="snafu", system="somewhere", platform=Platforms.PC)
-    snafu.active = False
-    fubar = await board.create_rescue(client="fubar", system="Fuelum", platform=Platforms.PC)
-    await fubar.add_rat(
-        Rat(uuid=None, name="some_unident", platform=Platforms.PC)
-    )
-    await fubar.add_rat(
-        Rat(uuid=None, name="some_other_unidentified", platform=Platforms.PC)
-    )
-
-    await snafu.add_rat(Rat(uuid=uuid4(), name="cl4p", platform=Platforms.PC))
-
-
 def render_rescue(rescue: Rescue, show_id: bool, show_unident: bool):
     template = env.get_template("rescue.jinja2")
 
