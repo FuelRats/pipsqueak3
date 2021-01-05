f3a119328cf99e60a0e986e38e980568bfd7e20e
Joshua Salzedo <joshuasalzedo@gmail.com>
2021-01-05 11:13:10 -0800

    Black pass over template dir

--- src/templates/__init__.py
+++ src/templates/__init__.py
@@ -26,45 +26,36 @@ from src.packages.utils.ratlib import Platforms, Status, Colors, color, bold, it
 async def render_rescue(rescue: Rescue, flags: ListFlags):
     template = env.get_template("rescue.jinja2")
 
-    return await template.render_async(
-        rescue=rescue, show_id=flags.show_uuids,
-        flags=flags
-    )
+    return await template.render_async(rescue=rescue, show_id=flags.show_uuids, flags=flags)
 
 
 async def render_board(board: RatBoard, **kwargs) -> str:
     template = env.get_template("board.jinja2")
-    return await (
-        template.render_async(
-            board=board,
-            **kwargs
-        )
-    )
+    return await (template.render_async(board=board, **kwargs))
 
 
 async def render_quotes(rescue: Rescue) -> str:
     template = env.get_template("quotation.jinja2")
 
-    return await template.render_async(
-        rescue=rescue
-    )
+    return await template.render_async(rescue=rescue)
 
 
 logger.debug("loading environment...")
 
 env = Environment(
-    loader=PackageLoader("src", "templates"), autoescape=select_autoescape(default=False),
-    enable_async=True
+    loader=PackageLoader("src", "templates"),
+    autoescape=select_autoescape(default=False),
+    enable_async=True,
 )
 # inject some objects into the environment so it can be accessed within the templates
-env.globals['Colors'] = Colors
-env.globals['color'] = color
-env.globals['bold'] = bold
-env.globals['italic'] = italic
-env.globals['Status'] = Status
-env.globals['render_rescue'] = render_rescue
-env.globals['render_board'] = render_board
-env.globals['Platforms'] = Platforms
-env.globals['now'] = pendulum.now
-env.globals['render_quotes'] = render_quotes
-env.globals['tz'] = pendulum.tz
+env.globals["Colors"] = Colors
+env.globals["color"] = color
+env.globals["bold"] = bold
+env.globals["italic"] = italic
+env.globals["Status"] = Status
+env.globals["render_rescue"] = render_rescue
+env.globals["render_board"] = render_board
+env.globals["Platforms"] = Platforms
+env.globals["now"] = pendulum.now
+env.globals["render_quotes"] = render_quotes
+env.globals["tz"] = pendulum.tz
